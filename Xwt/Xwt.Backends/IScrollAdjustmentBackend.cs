f67a09d50c95657a70784d10aa94bcaa2a87ea0b
Lluis Sanchez <lluis@xamarin.com>
2012-03-16 22:02:56 +0100

    Add some docs

--- Xwt/Xwt.Backends/IScrollAdjustmentBackend.cs
+++ Xwt/Xwt.Backends/IScrollAdjustmentBackend.cs
@@ -27,19 +27,81 @@ using System;
 
 namespace Xwt.Backends
 {
+	/// <summary>
+	/// A backend for a scrollbar
+	/// </summary>
 	public interface IScrollAdjustmentBackend: IBackend
 	{
+		/// <summary>
+		/// Called to initialize the backend
+		/// </summary>
+		/// <param name='eventSink'>
+		/// The event sink to be used to report events
+		/// </param>
 		void Initialize (IScrollAdjustmentEventSink eventSink);
+		
+		/// <summary>
+		/// Gets or sets the current position of the scrollbar.
+		/// </summary>
+		/// <value>
+		/// The position
+		/// </value>
+		/// <remarks>
+		/// Value is the position of top coordinate of the visible rect (or left for horizontal scrollbars).
+		/// So for example, if you set Value=35 and PageSize=100, the visible range will be 35 to 135.
+		/// Value must be >= LowerValue and <= (UpperValue - PageSize).
+		/// </remarks>
 		double Value { get; set; }
+		
+		/// <summary>
+		/// Gets or sets the lowest value of the scroll range.
+		/// </summary>
+		/// <value>
+		/// The lower value.
+		/// </value>
+		/// <remarks>It must be <= UpperValue</remarks>
 		double LowerValue { get; set; }
+		
+		/// <summary>
+		/// Gets or sets the highest value of the scroll range
+		/// </summary>
+		/// <value>
+		/// The upper value.
+		/// </value>
+		/// <remarks>It must be >= LowerValue</remarks>
 		double UpperValue { get; set; }
+		
+		/// <summary>
+		/// How much Value will be incremented when you click on the scrollbar to move
+		/// to the next page (when the scrollbar supports it)
+		/// </summary>
+		/// <value>
+		/// The page increment.
+		/// </value>
 		double PageIncrement { get; set; }
+		
+		/// <summary>
+		/// How much the Value is incremented/decremented when you click on the down/up button in the scrollbar
+		/// </summary>
+		/// <value>
+		/// The step increment.
+		/// </value>
 		double StepIncrement { get; set; }
+		
+		/// <summary>
+		/// Size of the visible range
+		/// </summary>
+		/// <remarks>
+		/// For example, if LowerValue=0, UpperValue=100, Value=25 and PageSize=50, the visible range will be 25 to 75
+		/// </remarks>
 		double PageSize { get; set; }
 	}
 
 	public interface IScrollAdjustmentEventSink
 	{
+		/// <summary>
+		/// Raised when the position of the scrollbar changes
+		/// </summary>
 		void OnValueChanged ();
 	}
 	
