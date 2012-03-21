f40e52f2a870a3f823a7b18af4dbbd348c45d6a9
lytico <lytico@users.sourceforge.net>
2012-03-21 13:27:30 +0100

    added ReferencePainter, refactored Samples.DrawingTransforms

--- Samples/Samples/DrawingTransforms.cs
+++ Samples/Samples/DrawingTransforms.cs
@@ -23,9 +23,7 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
-using System;
 using Xwt;
-using Xwt.Drawing;
 
 namespace Samples
 {
@@ -38,82 +36,23 @@ namespace Samples
 		protected override void OnDraw (Xwt.Drawing.Context ctx)
 		{
 			base.OnDraw (ctx);
+
+			ctx.Font = this.Font;
+
+			var p = new ReferencePainter ();
 			
-			// Simple rectangles
+			p.Rectangles (ctx);
 			
-			ctx.SetLineWidth (1);
-			ctx.Rectangle (100, 5, 10, 10);
-			ctx.SetColor (Color.Black);
-			ctx.Fill ();
+			p.LineDash (ctx);
 			
-			ctx.Rectangle (115, 5, 10, 10);
-			ctx.SetColor (Color.Black);
-			ctx.Stroke ();
+			p.Imaging (ctx);
 			
-			//
+			p.Rotation (ctx);
 			
-			ctx.SetLineWidth (3);
-			ctx.Rectangle (100, 20, 10, 10);
-			ctx.SetColor (Color.Black);
-			ctx.Fill ();
+			p.Curves (ctx);
 			
-			ctx.Rectangle (115, 20, 10, 10);
-			ctx.SetColor (Color.Black);
-			ctx.Stroke ();
+			p.Text (ctx);
 			
-			// Rectangle with hole
-			
-			ctx.Rectangle (10, 100, 40, 40);
-			ctx.MoveTo (45, 135);
-			ctx.RelLineTo (0, -20);
-			ctx.RelLineTo (-20, 0);
-			ctx.RelLineTo (0, 20);
-			ctx.ClosePath ();
-			ctx.SetColor (Color.Black);
-			ctx.Fill ();
-			
-			// Dashed lines
-			
-			ctx.SetLineDash (15, 10, 10, 5, 5);
-			ctx.Rectangle (100, 100, 100, 100);
-			ctx.Stroke ();
-			ctx.SetLineDash (0);
-			
-			ImageBuilder ib = new ImageBuilder (30, 30, ImageFormat.ARGB32);
-			ib.Context.Arc (15, 15, 15, 0, 360);
-			ib.Context.SetColor (new Color (1, 0, 1));
-			ib.Context.Rectangle (0, 0, 5, 5);
-			ib.Context.Fill ();
-			var img = ib.ToImage ();
-			ctx.DrawImage (img, 90, 90);
-			ctx.DrawImage (img, 90, 140, 50, 10);
-			
-			ctx.Arc (190, 190, 15, 0, 360);
-			ctx.SetColor (new Color (1, 0, 1, 0.4));
-			ctx.Fill ();
-			
-			ctx.Save ();
-			ctx.Translate (90, 220);
-			ctx.Pattern = new ImagePattern (img);
-			ctx.Rectangle (0, 0, 100, 70);
-			ctx.Fill ();
-			ctx.Restore ();
-			
-			ctx.Translate (30, 30);
-			double end = 270;
-			
-			for (double n = 0; n<=end; n += 5) {
-				ctx.Save ();
-				ctx.Rotate (n);
-				ctx.MoveTo (0, 0);
-				ctx.RelLineTo (30, 0);
-				double c = n / end;
-				ctx.SetColor (new Color (c, c, c));
-				ctx.Stroke ();
-				ctx.Restore ();
-			}
-			
-			ctx.ResetTransform ();
 		}
 	}
 }
