46a8db119431776749a1815a57052e35f7719d9c
ermau <me@ermau.com>
2012-03-26 14:38:22 -0400

    [WPF] Add Separator menu item

--- Xwt.WPF/Xwt.WPFBackend/WPFEngine.cs
+++ Xwt.WPF/Xwt.WPFBackend/WPFEngine.cs
@@ -49,6 +49,7 @@ namespace Xwt.WPFBackend
 			WidgetRegistry.RegisterBackend (typeof (Menu), typeof (MenuBackend));
 			WidgetRegistry.RegisterBackend (typeof (MenuItem), typeof (MenuItemBackend));
 			WidgetRegistry.RegisterBackend (typeof (CheckBoxMenuItem), typeof (CheckboxMenuItemBackend));
+			WidgetRegistry.RegisterBackend (typeof (SeparatorMenuItem), typeof (SeparatorMenuItemBackend));
 			WidgetRegistry.RegisterBackend (typeof (Table), typeof (BoxBackend));
 			WidgetRegistry.RegisterBackend (typeof (Box), typeof (BoxBackend));
 			WidgetRegistry.RegisterBackend (typeof (Label), typeof (LabelBackend));
