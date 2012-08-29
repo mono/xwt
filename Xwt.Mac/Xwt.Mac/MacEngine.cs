e1ed8e64579aa215ab8b30b6e8d492f6d066ae38
Jeremie Laval <jeremie.laval@gmail.com>
2012-08-29 16:29:22 +0100

    [Xwt.Mac] Register the default ListStore implementation

--- Xwt.Mac/Xwt.Mac/MacEngine.cs
+++ Xwt.Mac/Xwt.Mac/MacEngine.cs
@@ -88,6 +88,7 @@ namespace Xwt.Mac
 			WidgetRegistry.RegisterBackend (typeof(Xwt.Backends.IAlertDialogBackend), typeof(AlertDialogBackend));
 			WidgetRegistry.RegisterBackend (typeof(Xwt.StatusIcon), typeof(StatusIconBackend));
 			WidgetRegistry.RegisterBackend (typeof(Xwt.ProgressBar), typeof(ProgressBarBackend));
+			WidgetRegistry.RegisterBackend (typeof(Xwt.ListStore), typeof(Xwt.DefaultListStoreBackend));
 			WidgetRegistry.RegisterBackend (typeof(Xwt.Placement), typeof(BoxBackend));
 			WidgetRegistry.RegisterBackend (typeof(Xwt.Spinner), typeof(SpinnerBackend));
 			WidgetRegistry.RegisterBackend (typeof(Xwt.SpinButton), typeof(SpinButtonBackend));
