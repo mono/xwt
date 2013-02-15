c0f6c7387135f0ac2eb89218f214d053fbada7ec
Lluis Sanchez <lluis@xamarin.com>
2013-02-15 01:09:22 +0100

    Fixes in the context backend. Added missing files.

---
+++ Xwt.WPF/Xwt.WPFBackend/VisualCanvasBackend.cs
@@ -0,0 +1,98 @@
+﻿
+#if USE_WPF_RENDERING
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xwt.Backends;
+using System.Windows;
+using SWC = System.Windows.Controls;
+
+namespace Xwt.WPFBackend
+{
+	class CanvasBackend
+		: WidgetBackend, ICanvasBackend
+	{
+		#region ICanvasBackend Members
+
+		public CanvasBackend ()
+		{
+			Canvas = new ExCanvas ();
+			Canvas.RenderAction = Render;
+		}
+
+		private ExCanvas Canvas
+		{
+			get { return (ExCanvas) Widget; }
+			set { Widget = value; }
+		}
+
+		private ICanvasEventSink CanvasEventSink
+		{
+			get { return (ICanvasEventSink) EventSink; }
+		}
+
+		private void Render (System.Windows.Media.DrawingContext dc)
+		{
+			CanvasEventSink.OnDraw (new Xwt.WPFBackend.DrawingContext (Widget, dc), new Rectangle (0, 0, Widget.ActualWidth, Widget.ActualHeight));
+		}
+
+		public void QueueDraw ()
+		{
+			Canvas.InvalidateVisual ();
+		}
+
+		public void QueueDraw (Rectangle rect)
+		{
+			Canvas.InvalidateVisual ();
+		}
+
+		public void AddChild (IWidgetBackend widget, Rectangle bounds)
+		{
+			UIElement element = widget.NativeWidget as UIElement;
+			if (element == null)
+				throw new ArgumentException ();
+
+			if (!Canvas.Children.Contains (element))
+				Canvas.Children.Add (element);
+
+			SetChildBounds (widget, bounds);
+		}
+
+		public void SetChildBounds (IWidgetBackend widget, Rectangle bounds)
+		{
+			FrameworkElement element = widget.NativeWidget as FrameworkElement;
+			if (element == null)
+				throw new ArgumentException ();
+
+			double hratio = HeightPixelRatio;
+			double wratio = WidthPixelRatio;
+
+			SWC.Canvas.SetTop (element, bounds.Top * hratio);
+			SWC.Canvas.SetLeft (element, bounds.Left * wratio);
+
+			// We substract the widget margin here because the size we are assigning is the actual size, not including the WPF marings
+			var h = bounds.Height - ((WidgetBackend)widget).Frontend.Margin.VerticalSpacing;
+			var w = bounds.Width - ((WidgetBackend)widget).Frontend.Margin.HorizontalSpacing;
+
+			element.Height = (h > 0) ? h * hratio : 0;
+			element.Width = (w > 0) ? w * wratio : 0;
+
+			((FrameworkElement)widget.NativeWidget).UpdateLayout ();
+		}
+
+		public void RemoveChild (IWidgetBackend widget)
+		{
+			UIElement element = widget.NativeWidget as UIElement;
+			if (element == null)
+				throw new ArgumentException ();
+
+			Canvas.Children.Remove (element);
+		}
+
+		#endregion
+	}
+}
+
+#endif
---
+++ Xwt.WPF/Xwt.WPFBackend/VisualImageBuilderBackendHandler.cs
@@ -0,0 +1,80 @@
+//
+// ImageBuilderBackendHandler.cs
+//
+// Author:
+//       Eric Maupin <ermau@xamarin.com>
+//
+// Copyright (c) 2012 Xamarin, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#if USE_WPF_RENDERING
+
+using System.Drawing;
+using Xwt.Backends;
+using Xwt.Drawing;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Xwt.WPFBackend
+{
+	public class WpfImageBuilderBackendHandler
+		: ImageBuilderBackendHandler
+	{
+		class ImageBuilder: DrawingVisual
+		{
+			public int Width;
+			public int Height;
+			public DrawingContext Context;
+		}
+
+		public override object CreateImageBuilder (int width, int height, ImageFormat format)
+		{
+			return new ImageBuilder () {
+				Width = width,
+				Height = height
+			};
+		}
+
+		public override object CreateContext (object backend)
+		{
+			var visual = (ImageBuilder)backend;
+			visual.Context = new DrawingContext (visual, visual.RenderOpen ());
+			return visual.Context;
+		}
+
+		public override object CreateImage (object backend)
+		{
+			var visual = (ImageBuilder)backend;
+			var ratios = visual.GetPixelRatios ();
+			visual.Context.Dispose ();
+			var bmp = new RenderTargetBitmap (visual.Width, visual.Height, ratios.Height * 96, ratios.Width * 96, PixelFormats.Pbgra32);
+			bmp.Render(visual);
+			return bmp;
+		}
+
+		public override void Dispose (object backend)
+		{
+			var bmp = (ImageBuilder)backend;
+			bmp.Context.Dispose ();
+		}
+	}
+}
+
+#endif
