82600dcb671e7c3ab7d743f15e098f09c73d39de
Alex Corrado <alexc@xamarin.com>
2013-02-15 18:51:21 -0500

    [Gtk] Partial workaround for embedding an NSView inside a ScrolledWindow
    Manually reposition the NSView when the enclosing viewport is scrolled.
    Unfortunately, the NSView is still not clipped properly.
    
    This is only meant to be a stopgap until the gtk stack is fixed,
    at which point this commit should be reverted.

--- Xwt.Gtk/Xwt.GtkBackend/EmbeddedWidgetBackend.cs
+++ Xwt.Gtk/Xwt.GtkBackend/EmbeddedWidgetBackend.cs
@@ -24,12 +24,44 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 using System;
+using System.Reflection;
+using System.Drawing;
 using Xwt.Backends;
 
 namespace Xwt.GtkBackend
 {
 	public class EmbeddedWidgetBackend: WidgetBackend, IEmbeddedWidgetBackend
 	{
+
+		static Type nsViewType;
+		static Type NSViewType {
+			get {
+				if (nsViewType == null)
+					nsViewType = Type.GetType ("MonoMac.AppKit.NSView, MonoMac", false);
+				return nsViewType;
+			}
+		}
+
+		static PropertyInfo nsViewFrame;
+		static PropertyInfo NSViewFrame {
+			get {
+				if (nsViewFrame == null)
+					nsViewFrame = NSViewType.GetProperty ("Frame");
+				return nsViewFrame;
+			}
+		}
+
+		static PropertyInfo nsViewHidden;
+		static PropertyInfo NSViewHidden {
+			get {
+				if (nsViewHidden == null)
+					nsViewHidden = NSViewType.GetProperty ("Hidden");
+				return nsViewHidden;
+			}
+		}
+
+		object nativeWidget;
+
 		public EmbeddedWidgetBackend ()
 		{
 		}
@@ -42,12 +74,77 @@ namespace Xwt.GtkBackend
 			}
 
 			// Check if it is an NSView
-			Type nsView = Type.GetType ("MonoMac.AppKit.NSView, MonoMac", false);
-			if (nsView != null && nsView.IsInstanceOfType (nativeWidget)) {
-				Widget = GtkMacInterop.NSViewToGtkWidget (nativeWidget);
+			if (NSViewType != null && NSViewType.IsInstanceOfType (nativeWidget)) {
+				this.nativeWidget = nativeWidget;
+
+				Gtk.Widget wrapper = GtkMacInterop.NSViewToGtkWidget (nativeWidget);
+				wrapper.Show ();
+
+				Widget = new Gtk.EventBox {
+					VisibleWindow = false,
+					Child = wrapper
+				};
+				Widget.ParentSet += HandleParentSet;
+				Widget.VisibilityNotifyEvent += HandleVisibilityNotifyEvent;
 				Widget.Show ();
 				return;
 			}
+		}
+
+		void HandleVisibilityNotifyEvent (object o, Gtk.VisibilityNotifyEventArgs args)
+		{
+			NSViewHidden.SetValue (nativeWidget, args.Event.State == Gdk.VisibilityState.FullyObscured, null);
+		}
+
+		double hvalue, vvalue;
+		void HandleParentSet (object o, Gtk.ParentSetArgs args)
+		{
+			// workaround a bug when GtkNSView is inside a ScrolledWindow
+
+			var w = args.PreviousParent;
+			while (w != null) {
+				w.ParentSet -= HandleParentSet;
+				w = w.Parent;
+			}
+
+			w = ((Gtk.Widget)o).Parent;
+			while (w != null) {
+				w.ParentSet += HandleParentSet;
+
+				var viewport = w as Gtk.Viewport;
+				if (viewport != null) {
+					viewport.ScrollAdjustmentsSet += (s, evt) => {
+						TrackScrollAdjustment (evt.Hadjustment, true);
+						TrackScrollAdjustment (evt.Vadjustment, false);
+					};
+					TrackScrollAdjustment (viewport.Hadjustment, true);
+					TrackScrollAdjustment (viewport.Vadjustment, false);
+				}
+				w = w.Parent;
+			}
+		}
+
+		void TrackScrollAdjustment (Gtk.Adjustment adj, bool isHorizontal)
+		{
+			if (adj == null)
+				return;
+			if (isHorizontal)
+				hvalue = adj.Value;
+			else
+				vvalue = adj.Value;
+
+			adj.ValueChanged += delegate {
+				var frame = (RectangleF) NSViewFrame.GetValue (nativeWidget, null);
+				var newValue = adj.Value;
+				if (isHorizontal) {
+					frame.X -= (float)(newValue - hvalue);
+					hvalue = newValue;
+				} else {
+					frame.Y -= (float)(newValue - vvalue);
+					vvalue = newValue;
+				}
+				NSViewFrame.SetValue (nativeWidget, frame, null);
+			};
 		}
 	}
 }
