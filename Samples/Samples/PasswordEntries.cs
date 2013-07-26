6591f97f5f68df8e8388dc83322cf4cda8c84e64
Bojan Rajkovic <brajkovic@xamarin.com>
2013-07-26 14:10:28 -0400

    Implement password entry for GTK and WPF.

--- Samples/Samples/PasswordEntries.cs
+++ Samples/Samples/PasswordEntries.cs
@@ -1,21 +1,21 @@
-// 
-// DefaultNaturalSizes.cs
-//  
+//
+// PasswordEntry.cs
+//
 // Author:
-//       Lluis Sanchez <lluis@xamarin.com>
-// 
-// Copyright (c) 2012 Xamarin Inc
-// 
+//       Bojan Rajkovic <brajkovic@xamarin.com>
+//
+// Copyright (c) 2013 Xamarin Inc.
+//
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in
 // all copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -24,17 +24,24 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 using System;
+using Xwt;
 
-namespace Xwt.Backends
+namespace Samples
 {
-	static class DefaultNaturalSizes
+	public class PasswordEntries : VBox
 	{
-		public static Size ComboBox = new Size (100, 0);
-		public static Size ListView = new Size (100, 100);
-		public static Size ScrollView = new Size (50, 50);
-		public static Size TextEntry = new Size (100, 0);
-		public static Size PasswordEntry = new Size (100, 0);
-		public static Size TreeView = new Size (100, 100);
+		public PasswordEntries ()
+		{
+			PasswordEntry te1 = new PasswordEntry ();
+			PackStart (te1);
+			Button b = new Button ("Show password");
+			Label l = new Label ();
+			b.Clicked += (sender, e) => {
+				l.Text = ("Password is: " + te1.Password);
+			};
+			PackStart (b);
+			PackStart (l);
+		}
 	}
 }
 
---
+++ Xwt.WPF/Xwt.WPFBackend/PasswordEntryBackend.cs
@@ -0,0 +1,66 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+using Xwt.WPFBackend;
+using Xwt.Backends;
+
+namespace Xwt.WPFBackend
+{
+	class PasswordEntryBackend : WidgetBackend, IPasswordEntryBackend
+	{
+		public PasswordEntryBackend ()
+		{
+			Widget = new PasswordBox ();
+		}
+
+		protected PasswordBox PasswordBox
+		{
+			get { return (PasswordBox) Widget; }
+		}
+
+		public string Password
+		{
+			get { return PasswordBox.Password; }
+			set { PasswordBox.Password = value; }
+		}
+
+		public override void EnableEvent (object eventId)
+		{
+			base.EnableEvent (eventId);
+
+			if (eventId is PasswordEntryEvent) 
+			{
+				switch ((PasswordEntryEvent) eventId) 
+				{
+					case PasswordEntryEvent.Changed:
+						PasswordBox.PasswordChanged += OnPasswordChanged;
+						break;
+				}
+			}
+		}
+
+		public override void DisableEvent (object eventId)
+		{
+			base.DisableEvent (eventId);
+
+			if (eventId is PasswordEntryEvent)
+			{
+				switch ((PasswordEntryEvent)eventId)
+				{
+					case PasswordEntryEvent.Changed:
+						PasswordBox.PasswordChanged -= OnPasswordChanged;
+						break;
+				}
+			}
+		}
+
+		protected new IPasswordEntryEventSink EventSink {
+			get { return (IPasswordEntryEventSink) base.EventSink; }
+		}
+
+		void OnPasswordChanged (object s, RoutedEventArgs e)
+		{
+			Context.InvokeUserCode (EventSink.OnChanged);
+		}
+	}
+}
