c51569338b1e2243c385de3380c0aeb5a4ec1342
Lluis Sanchez <lluis@xamarin.com>
2013-09-24 09:35:50 +0200

    Improved ListView sample

--- Samples/Samples/ListView2.cs
+++ Samples/Samples/ListView2.cs
@@ -6,29 +6,33 @@ namespace Samples
 {
 	public class ListView2: VBox
 	{
-		DataField<bool> editable;
-		DataField<bool> nonEditable;
-
 		public ListView2 ()
 		{
 			ListView list = new ListView ();
-			editable = new DataField<bool> ();
-			nonEditable = new DataField<bool> ();
+			var editableActiveField = new DataField<bool> ();
+			var nonEditableActiveField = new DataField<bool> ();
 			var textField = new DataField<string> ();
-			ListStore store = new ListStore (editable, nonEditable, textField);
+			var textField2 = new DataField<string> ();
+			var editableField = new DataField<bool> ();
+
+			ListStore store = new ListStore (editableActiveField, nonEditableActiveField, textField, textField2, editableField);
 			list.DataSource = store;
 
-			list.Columns.Add (new ListViewColumn("Editable", new CheckBoxCellView { Editable = true, ActiveField = editable }));
-			list.Columns.Add (new ListViewColumn("Not Editable", new CheckBoxCellView { Editable = false, ActiveField = nonEditable }));
+			list.Columns.Add (new ListViewColumn("Editable", new CheckBoxCellView { Editable = true, ActiveField = editableActiveField }));
+			list.Columns.Add (new ListViewColumn("Not Editable", new CheckBoxCellView { Editable = false, ActiveField = nonEditableActiveField }));
 			list.Columns.Add (new ListViewColumn("Editable", new TextCellView { Editable = true, TextField = textField }));
+			list.Columns.Add (new ListViewColumn("Somewhat Editable", new TextCellView { EditableField = editableField, TextField = textField2 }));
 
 			Random rand = new Random ();
 			
 			for (int n=0; n<100; n++) {
 				var r = store.AddRow ();
-				store.SetValue (r, editable, rand.Next(0, 2) == 0);
-				store.SetValue (r, nonEditable, rand.Next(0, 2) == 0);
+				store.SetValue (r, editableActiveField, rand.Next(0, 2) == 0);
+				store.SetValue (r, nonEditableActiveField, rand.Next(0, 2) == 0);
 				store.SetValue (r, textField, n.ToString ());
+				var edit = (n % 2) == 0;
+				store.SetValue (r, editableField, edit);
+				store.SetValue (r, textField2, edit ? "editable" : "not editable");
 			}
 			PackStart (list, true);
 		}
