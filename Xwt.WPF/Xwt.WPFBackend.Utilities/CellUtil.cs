b65b6a52324f4609b4ff5a1f64f05a77cb2336bf
steffenWi <steffen-winkler@gmx.de>
2013-09-24 21:18:47 +0200

    Replaced spaces with tabs

--- Xwt.WPF/Xwt.WPFBackend.Utilities/CellUtil.cs
+++ Xwt.WPF/Xwt.WPFBackend.Utilities/CellUtil.cs
@@ -55,41 +55,41 @@ namespace Xwt.WPFBackend.Utilities
 		{
 			TextCellView textView = view as TextCellView;
 			if (textView != null) {
-                // if it's an editable textcontrol, use a TextBox, if not use a TextBlock. Reason for this is that 
-                // a user usually expects to be able to edit a text if a text cursor is appearing above a field.
-                FrameworkElementFactory factory;
-                if (textView.EditableField == null)
-                {
-                    if (textView.Editable)
-                    {
-                        factory = new FrameworkElementFactory(typeof(SWC.TextBox));
-                        factory.SetValue(FrameworkElement.MarginProperty, CellMargins);
-                        factory.SetValue(SWC.TextBox.IsReadOnlyProperty, false);
-                        if (textView.TextField != null)
-                        {
-                            factory.SetBinding(SWC.TextBox.TextProperty, new Binding(dataPath + "[" + textView.TextField.Index + "]"));
-                        }
-                    }
-                    else
-                    {
-                        factory = new FrameworkElementFactory(typeof(SWC.TextBlock));
-                        factory.SetValue(FrameworkElement.MarginProperty, CellMargins);
-                        if (textView.TextField != null)
-                        {
-                            factory.SetBinding(SWC.TextBlock.TextProperty, new Binding(dataPath + "[" + textView.TextField.Index + "]"));
-                        }
-                    }
-                }
-                else
-                {
-                    factory = new FrameworkElementFactory(typeof(SWC.TextBox));
-                    factory.SetValue(FrameworkElement.MarginProperty, CellMargins);
-                    factory.SetBinding(SWC.TextBox.IsEnabledProperty, new Binding(dataPath + "[" + textView.EditableField.Index + "]"));
-                    if (textView.TextField != null)
-                    {
-                        factory.SetBinding(SWC.TextBox.TextProperty, new Binding(dataPath + "[" + textView.TextField.Index + "]"));
-                    }
-                }
+				// if it's an editable textcontrol, use a TextBox, if not use a TextBlock. Reason for this is that 
+				// a user usually expects to be able to edit a text if a text cursor is appearing above a field.
+				FrameworkElementFactory factory;
+				if (textView.EditableField == null)
+				{
+					if (textView.Editable)
+					{
+						factory = new FrameworkElementFactory(typeof(SWC.TextBox));
+						factory.SetValue(FrameworkElement.MarginProperty, CellMargins);
+						factory.SetValue(SWC.TextBox.IsReadOnlyProperty, false);
+						if (textView.TextField != null)
+						{
+							factory.SetBinding(SWC.TextBox.TextProperty, new Binding(dataPath + "[" + textView.TextField.Index + "]"));
+						}
+					}
+					else
+					{
+						factory = new FrameworkElementFactory(typeof(SWC.TextBlock));
+						factory.SetValue(FrameworkElement.MarginProperty, CellMargins);
+						if (textView.TextField != null)
+						{
+							factory.SetBinding(SWC.TextBlock.TextProperty, new Binding(dataPath + "[" + textView.TextField.Index + "]"));
+						}
+					}
+				}
+				else
+				{
+					factory = new FrameworkElementFactory(typeof(SWC.TextBox));
+					factory.SetValue(FrameworkElement.MarginProperty, CellMargins);
+					factory.SetBinding(SWC.TextBox.IsEnabledProperty, new Binding(dataPath + "[" + textView.EditableField.Index + "]"));
+					if (textView.TextField != null)
+					{
+						factory.SetBinding(SWC.TextBox.TextProperty, new Binding(dataPath + "[" + textView.TextField.Index + "]"));
+					}
+				}
 
 				return factory;
 			}
@@ -121,27 +121,27 @@ namespace Xwt.WPFBackend.Utilities
 			}
 			
 			CheckBoxCellView cellView = view as CheckBoxCellView;
-            		if (cellView != null)
-            		{
-                		FrameworkElementFactory factory = new FrameworkElementFactory(typeof(SWC.CheckBox));
-                        if (cellView.EditableField == null)
-                        {
-                            factory.SetValue(FrameworkElement.IsEnabledProperty, cellView.Editable);
-                        }
-                        else
-                        {
-                            factory.SetBinding(SWC.CheckBox.IsEnabledProperty, new Binding(dataPath + "[" + cellView.EditableField.Index + "]"));
-                        }
+					if (cellView != null)
+					{
+						FrameworkElementFactory factory = new FrameworkElementFactory(typeof(SWC.CheckBox));
+						if (cellView.EditableField == null)
+						{
+							factory.SetValue(FrameworkElement.IsEnabledProperty, cellView.Editable);
+						}
+						else
+						{
+							factory.SetBinding(SWC.CheckBox.IsEnabledProperty, new Binding(dataPath + "[" + cellView.EditableField.Index + "]"));
+						}
 
-                        factory.SetValue(SWC.CheckBox.IsThreeStateProperty, cellView.AllowMixed);
-                		factory.SetValue(FrameworkElement.MarginProperty, CellMargins);
-                		if (cellView.ActiveField != null)
-                		{
-                    			factory.SetBinding(SWC.CheckBox.IsCheckedProperty, new Binding(dataPath + "[" + cellView.ActiveField.Index + "]"));
-                		}
+						factory.SetValue(SWC.CheckBox.IsThreeStateProperty, cellView.AllowMixed);
+						factory.SetValue(FrameworkElement.MarginProperty, CellMargins);
+						if (cellView.ActiveField != null)
+						{
+								factory.SetBinding(SWC.CheckBox.IsCheckedProperty, new Binding(dataPath + "[" + cellView.ActiveField.Index + "]"));
+						}
 
-                		return factory;
-            		}
+						return factory;
+					}
 
 			throw new NotImplementedException ();
 		}
