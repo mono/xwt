d41c0f1b037311ebdffc4bda40aff82f5480548e
Lluis Sanchez <lluis@xamarin.com>
2013-12-03 20:40:39 +0100

    [WPF] Fixed 9-patch rendering issues
    And added tests.

--- Testing/WpfTestRunner/Program.cs
+++ Testing/WpfTestRunner/Program.cs
@@ -12,15 +12,14 @@ namespace WpfTestRunner
 		static void Main (string[] args)
 		{
 			Xwt.Application.Initialize (Xwt.ToolkitType.Wpf);
-			ReferenceImageManager.Init ("GtkTestRunner");
+			ReferenceImageManager.Init ("WpfTestRunner");
 
 			var list = new List<string> (args);
 			list.Add ("-domain=None");
 			list.Add ("-noshadow");
 			list.Add ("-nothread");
 			list.Add ("-xml=result.xml");
-//			list.Add ("-fixture=Xwt.WindowTests");
-//			list.Add ("-run=Xwt.WindowTests.DefaultSize");
+//			list.Add ("/run=Xwt.NinePatchTests");
 			list.Add (typeof (Program).Assembly.Location);
 			NUnit.ConsoleRunner.Runner.Main (list.ToArray ());
 			ReferenceImageManager.ShowImageVerifier ();
--- Xwt.WPF/Xwt.WPFBackend/ImageHandler.cs
+++ Xwt.WPF/Xwt.WPFBackend/ImageHandler.cs
@@ -418,14 +418,16 @@ namespace Xwt.WPFBackend
 		{
 			ImageDescription idesc = new ImageDescription () {
 				Alpha = 1,
-				Size = new Size (width * scaleFactor, height * scaleFactor)
+				Size = new Size (width, height)
 			};
 			SWM.DrawingVisual visual = new SWM.DrawingVisual ();
 			using (SWM.DrawingContext ctx = visual.RenderOpen ()) {
-				Draw (actx, ctx, 1, 0, 0, idesc);
+				ctx.PushTransform (new ScaleTransform (scaleFactor, scaleFactor));
+				Draw (actx, ctx, scaleFactor, 0, 0, idesc);
+				ctx.Pop ();
 			}
 
-			SWMI.RenderTargetBitmap bmp = new SWMI.RenderTargetBitmap ((int)idesc.Size.Width, (int)idesc.Size.Height, 96, 96, PixelFormats.Pbgra32);
+			SWMI.RenderTargetBitmap bmp = new SWMI.RenderTargetBitmap ((int)(width * scaleFactor), (int)(height * scaleFactor), 96, 96, PixelFormats.Pbgra32);
 			bmp.Render (visual);
 
 			var f = new ImageFrame (bmp, width, height);
