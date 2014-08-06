aa0b22270023579cec7388b7eafcda1a53b0fc4f
Alan McGovern <alan@xamarin.com>
2014-08-06 08:50:26 -0400

    [Xwt.Wpf] Add ComboBox support to PlaceholderTextAdorner
    We also have cases where comboboxes need to be adorned with
    placeholder text.

--- Xwt.WPF/Xwt.WPFBackend/PlaceholderTextAdorner.cs
+++ Xwt.WPF/Xwt.WPFBackend/PlaceholderTextAdorner.cs
@@ -5,15 +5,22 @@ using System.Linq;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Media;
 
 namespace Xwt.WPFBackend
 {
-	class PlaceholderTextAdorner: Adorner
+	public class PlaceholderTextAdorner: Adorner
 	{
 		public static readonly DependencyProperty PlaceholderTextProperty = DependencyProperty.Register ("PlaceholderText", typeof (string), typeof (PlaceholderTextAdorner), new PropertyMetadata (OnPlaceHolderTextChanged));
+		public static readonly DependencyProperty ComboBoxTextChangedProperty = DependencyProperty.Register ("ComboBoxTextChanged", typeof (string), typeof (PlaceholderTextAdorner), new PropertyMetadata (OnComboBoxTextChanged));
 		
+		static void OnComboBoxTextChanged (DependencyObject obj, DependencyPropertyChangedEventArgs args)
+		{
+			((PlaceholderTextAdorner)obj).AdornedWidgetChanged (obj, new RoutedEventArgs ());
+		}
+
 		static void OnPlaceHolderTextChanged (DependencyObject obj, DependencyPropertyChangedEventArgs args)
 		{
 			((PlaceholderTextAdorner)obj).InvalidateVisual ();
@@ -27,19 +34,42 @@ namespace Xwt.WPFBackend
 			get { return AdornedElement as TextBox; }
 		}
 
+		System.Windows.Controls.ComboBox AdornedComboBox {
+			get { return AdornedElement as System.Windows.Controls.ComboBox; }
+		}
+
 		public string PlaceholderText {
 			get { return (string) GetValue (PlaceholderTextProperty); }
 			set { SetValue (PlaceholderTextProperty, value); }
 		}
 
-		public PlaceholderTextAdorner (UIElement adornedElement)
+		public PlaceholderTextAdorner (System.Windows.Controls.ComboBox adornedElement)
 			: base (adornedElement)
+		{
+			Initialize ();
+		}
+
+		public PlaceholderTextAdorner (System.Windows.Controls.PasswordBox adornedElement)
+			: base (adornedElement)
+		{
+			Initialize ();
+		}
+
+		public PlaceholderTextAdorner (System.Windows.Controls.TextBox adornedElement)
+			: base (adornedElement)
+		{
+			Initialize ();
+		}
+
+		void Initialize ()
 		{
 			IsHitTestVisible = false;
 			if (AdornedPasswordBox != null) {
 				AdornedPasswordBox.PasswordChanged += AdornedWidgetChanged;
 			} else if (AdornedTextBox != null) {
 				AdornedTextBox.TextChanged += AdornedWidgetChanged;
+			} else if (AdornedComboBox != null) {
+				SetBinding (ComboBoxTextChangedProperty, new Binding { Source = AdornedComboBox, Path = new PropertyPath ("Text")  });
 			}
 		}
 
@@ -49,6 +79,8 @@ namespace Xwt.WPFBackend
 				Visibility = string.IsNullOrEmpty (AdornedPasswordBox.Password) ? Visibility.Visible: Visibility.Hidden;
 			if (AdornedTextBox != null)
 				Visibility = string.IsNullOrEmpty (AdornedTextBox.Text) ? Visibility.Visible : Visibility.Hidden;
+			if (AdornedComboBox != null)
+				Visibility = string.IsNullOrEmpty (AdornedComboBox.Text) ? Visibility.Visible : Visibility.Hidden;
 		}
 
 		protected override void OnRender(System.Windows.Media.DrawingContext drawingContext)
@@ -65,12 +97,19 @@ namespace Xwt.WPFBackend
 				typeFace = AdornedPasswordBox.FontFamily.GetTypefaces ().FirstOrDefault ();
 				padding = 6;
 			}
-			else {
+			else if (AdornedTextBox != null) {
 				alignment = AdornedTextBox.ReadLocalValue (TextBox.TextAlignmentProperty) !=DependencyProperty.UnsetValue ? AdornedTextBox.TextAlignment : ConvertAlignment (AdornedTextBox.HorizontalContentAlignment);
 				flowDirection = AdornedTextBox.FlowDirection;
 				fontSize = AdornedTextBox.FontSize;
 				typeFace = AdornedTextBox.FontFamily.GetTypefaces ().FirstOrDefault ();
 				padding = 6;
+			} else {
+				alignment = ConvertAlignment (AdornedComboBox.HorizontalContentAlignment);
+				flowDirection = AdornedComboBox.FlowDirection;
+				fontSize = AdornedComboBox.FontSize;
+				typeFace = AdornedComboBox.FontFamily.GetTypefaces ().FirstOrDefault ();
+				padding = 6;
+
 			}
 			var text = new System.Windows.Media.FormattedText (PlaceholderText ?? "", CultureInfo.CurrentCulture, flowDirection, typeFace, fontSize, System.Windows.Media.Brushes.LightGray) {
 				TextAlignment = alignment
