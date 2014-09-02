feeb32adc4721332b1ff86ab0df9c97d3c72f7b6
Vsevolod Kukol <sevo@sevo.org>
2014-09-02 09:47:57 +0200

    [Wpf] add SpinButton custom formatting support

--- Xwt.WPF/Xwt.WPFBackend/WindowsSpinButton.xaml.cs
+++ Xwt.WPF/Xwt.WPFBackend/WindowsSpinButton.xaml.cs
@@ -88,7 +88,16 @@ namespace Xwt.WPFBackend
         public double Value
         {
             get { return (double)GetValue(ValueProperty); }
-            set { SetValue(ValueProperty, Math.Round(value, DecimalPlaces)); }
+            set {
+                SetValue(ValueProperty, Math.Round(value, DecimalPlaces));
+                UpdateTextbox ();
+            }
+        }
+
+        public string Text
+        {
+            get { return textBox.Text; }
+            set { textBox.Text = value; }
         }
 
         public static readonly DependencyProperty WrapProperty =
@@ -230,17 +239,28 @@ namespace Xwt.WPFBackend
             Grid.SetColumn(buttonsGrid, 1);
             Content = mainGrid;
         }
+        
+        public event EventHandler<WidgetEventArgs> OnValueOutput;
+        public event EventHandler<SpinButtonInputEventArgs> OnValueInput;
+        
         private bool valueChangedByUser = false;
         private void UpdateTextbox()
         {
-            if (IsIndeterminate && !valueChangedByUser)
-                textBox.Text = IndeterminateMessage;
-            else
-                textBox.Text = string.Format("{0:N" + DecimalPlaces + "}", Value);
             if (!Wrap)
             {
                 buttonUp.IsEnabled = Value != MaximumValue && IsEnabled;
                 buttonDown.IsEnabled = Value != MinimumValue && IsEnabled;
+            }
+            if (IsIndeterminate && !valueChangedByUser)
+                textBox.Text = IndeterminateMessage;
+            else {
+                if (OnValueOutput != null) {
+                    var argsOutput = new WidgetEventArgs ();
+                    OnValueOutput (this, argsOutput);
+                    if (argsOutput.Handled)
+                        return;
+                }
+                textBox.Text = string.Format ("{0:N" + DecimalPlaces + "}", Value);
             }
         }
 
@@ -252,15 +272,24 @@ namespace Xwt.WPFBackend
 
         private void parseTextBox()
         {
-            //Simulating GTK behavior by ignoring Culture and GroupSeperator
-            string stringValue = textBox.Text;
-            string[] stringSplitedValue = stringValue.Split('.', ',');
-            if (stringSplitedValue.Length > 1)
-                stringValue = stringSplitedValue[0] + "." + stringSplitedValue[1];
-            else
-                stringValue = stringSplitedValue[0];
-            double newValue = 0;
-            if (double.TryParse(stringValue, System.Globalization.NumberStyles.Number, CultureInfo.InvariantCulture, out newValue))
+            double newValue = double.NaN;
+            if (OnValueInput != null) {
+                var argsInput = new SpinButtonInputEventArgs ();
+                OnValueInput (this, argsInput);
+                if (argsInput.Handled)
+                    newValue = argsInput.NewValue;
+            }
+            if (newValue == double.NaN) {
+                //Simulating GTK behavior by ignoring Culture and GroupSeperator
+                string stringValue = textBox.Text;
+                string[] stringSplitedValue = stringValue.Split ('.', ',');
+                if (stringSplitedValue.Length > 1)
+                    stringValue = stringSplitedValue [0] + "." + stringSplitedValue [1];
+                else
+                    stringValue = stringSplitedValue [0];
+                double.TryParse (stringValue, System.Globalization.NumberStyles.Number, CultureInfo.InvariantCulture, out newValue);
+            }
+            if (newValue != double.NaN)
             {
                 if (newValue > MaximumValue)
                     newValue = MaximumValue;
