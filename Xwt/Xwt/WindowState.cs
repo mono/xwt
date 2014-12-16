d3f9c3a8183065f0b6dd1666c9aaabb1b23d72d2
René Paw Christensen <spam1@rpc-scandinavia.dk>
2014-12-16 15:52:42 +0100

    Ifonify/Minimize windows

---
+++ Xwt/Xwt/WindowState.cs
@@ -0,0 +1,18 @@
+//
+// WindowState.cs
+//
+// Author:
+//       Jérémie Laval <jeremie.laval@xamarin.com>
+//
+// Copyright (c) 2012 Xamarin, Inc.
+using System;
+
+namespace Xwt
+{
+	public enum WindowState
+	{
+		Normal,
+		Icon,				// Minimized.
+		FullScreen		// Maximized.
+	}
+}
