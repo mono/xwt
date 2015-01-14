9ef43af4a8b04dc5a0b9ad8982f9ed073f9135d4
Vsevolod Kukol <sevo@sevo.org>
2015-01-14 16:26:10 +0100

    Add FontSelector example to samples app

---
+++ TestApps/Samples/Samples/FontSelectorSample.cs
@@ -0,0 +1,48 @@
+﻿//
+// FontSelectorSample.cs
+//
+// Author:
+//       Vsevolod Kukol <sevo@sevo.org>
+//
+// Copyright (c) 2015 Vsevolod Kukol
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+using System;
+using Xwt;
+
+namespace Samples
+{
+	public class FontSelectorSample: VBox
+	{
+		public FontSelectorSample ()
+		{
+			FontSelector sel = new FontSelector ();
+			sel.PreviewText = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy";
+			Label preview = new Label ("Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy");
+			preview.Ellipsize = EllipsizeMode.End;
+
+			sel.FontChanged += (sender, e) => preview.Font = sel.SelectedFont;
+
+			PackStart (sel);
+			PackStart (new HSeparator());
+			PackStart (preview);
+		}
+	}
+}
+
