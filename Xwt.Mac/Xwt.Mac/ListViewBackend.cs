c3ebb00d0d33c8a571f8988f9948b04cfa810d5c
Vsevolod Kukol <sevo@sevo.org>
2015-01-16 08:58:51 +0100

    [Mac] fix empty ListViewBackend.SelectedRows
    Add test for an emty selection.

--- Xwt.Mac/Xwt.Mac/ListViewBackend.cs
+++ Xwt.Mac/Xwt.Mac/ListViewBackend.cs
@@ -94,9 +94,11 @@ namespace Xwt.Mac
 		public int[] SelectedRows {
 			get {
 				int[] sel = new int [Table.SelectedRowCount];
-				int i = 0;
-				foreach (int r in Table.SelectedRows)
-					sel [i++] = r;
+				if (sel.Length > 0) {
+					int i = 0;
+					foreach (int r in Table.SelectedRows)
+						sel [i++] = r;
+				}
 				return sel;
 			}
 		}
