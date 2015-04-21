mono/xwt

commit e1275a299522ff4db93e4b464a81211d192816c1

[Xwt.Mac] Don't crash if the TextEntry is destroyed while inside a KeyUp
There are two changes here:
1) We run our internal logic before letting the rest of the toolkit handle
the KeyUp event. This is so we always emit the event when the cursor position
changes, regardless of whether or not the user destroys the widget inside the
KeyUp handler they attached.

2) We null check 'CurrentEditor' and only access it if it's non-null. This
prevents a crash when destroying the editor inside the KeyUp handlers and
matches what the rest of the code does.

Xwt.XamMac/Xwt.Mac/TextEntryBackend.cs
@@ -275,12 +275,12 @@ namespace Xwt.Mac
 		int cachedCursorPosition;
 		public override void KeyUp (NSEvent theEvent)
 		{
-			base.KeyUp (theEvent);
-			if (cachedCursorPosition != CurrentEditor.SelectedRange.Location)
+			if (CurrentEditor!= null && cachedCursorPosition != CurrentEditor.SelectedRange.Location)
+				cachedCursorPosition = (int)CurrentEditor.SelectedRange.Location;
 				context.InvokeUserCode (delegate {
 				eventSink.OnSelectionChanged ();
 			});
-			cachedCursorPosition = (int)CurrentEditor.SelectedRange.Location;
+			base.KeyUp (theEvent);
 		}
 	}
 }

commit d89fe856c8c0736746eff52c0464a93195d42461

[Xwt.XamMac] Fix a regression in e1275a29
There were some missing braces, so commit e1275a29 resulted in the
event *always* emitting. Oops.

Xwt.XamMac/Xwt.Mac/TextEntryBackend.cs
@@ -275,11 +275,12 @@ namespace Xwt.Mac
 		int cachedCursorPosition;
 		public override void KeyUp (NSEvent theEvent)
 		{
-			if (CurrentEditor!= null && cachedCursorPosition != CurrentEditor.SelectedRange.Location)
+			if (CurrentEditor!= null && cachedCursorPosition != CurrentEditor.SelectedRange.Location) {
 				cachedCursorPosition = (int)CurrentEditor.SelectedRange.Location;
 				context.InvokeUserCode (delegate {
-				eventSink.OnSelectionChanged ();
-			});
+					eventSink.OnSelectionChanged ();
+				});
+			}
 			base.KeyUp (theEvent);
 		}
 	}