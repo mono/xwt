a697dacbf657f6de0928e9582414872ee8580528
Alex Corrado <alexc@xamarin.com>
2015-05-27 19:22:38 -0400

    [Mac] Fix sizing of embedded (wrapped) native NSViews
    The EmbeddedNativeWidget creates its own parent NSView and we need
    to configure it to correctly size the child (the actual embedded view)

--- Xwt.XamMac/Xwt.Mac/EmbedNativeWidgetBackend.cs
+++ Xwt.XamMac/Xwt.Mac/EmbedNativeWidgetBackend.cs
@@ -51,6 +51,11 @@ namespace Xwt.Mac
 				innerView.RemoveFromSuperview ();
 			innerView = aView;
 			innerView.Frame = Widget.Bounds;
+
+			innerView.AutoresizingMask = NSViewResizingMask.WidthSizable | NSViewResizingMask.HeightSizable;
+			innerView.TranslatesAutoresizingMaskIntoConstraints = true;
+			Widget.AutoresizesSubviews = true;
+
 			Widget.AddSubview (innerView);
 		}
 	}
