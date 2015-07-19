a582219dcff564becb5e4feeffe24a2c8ec72d37
Sebastian Krysmanski <noreply@manski.net>
2015-07-19 11:03:28 +0200

    Fixed MonoMac compile break

---
+++ Xwt.Mac/NSStringAttributeKey.cs
@@ -0,0 +1,43 @@
+﻿using MonoMac.Foundation;
+
+namespace Xwt.Mac
+{
+	public static class NSStringAttributeKey
+	{
+		public static NSString ForegroundColor
+		{
+			get { return NSAttributedString.ForegroundColorAttributeName; }
+		}
+
+		public static NSString UnderlineStyle
+		{
+			get { return NSAttributedString.UnderlineStyleAttributeName; }
+		}
+
+		public static NSString BackgroundColor
+		{
+			get { return NSAttributedString.BackgroundColorAttributeName; }
+		}
+
+		public static NSString Obliqueness
+		{
+			get { return NSAttributedString.ObliquenessAttributeName; }
+		}
+
+		public static NSString Link
+		{
+			get { return NSAttributedString.LinkAttributeName; }
+		}
+
+		public static NSString StrikethroughStyle
+		{
+			get { return NSAttributedString.StrikethroughStyleAttributeName; }
+		}
+
+		public static NSString Font
+		{
+			get { return NSAttributedString.FontAttributeName; }
+		}
+	}
+}
+
