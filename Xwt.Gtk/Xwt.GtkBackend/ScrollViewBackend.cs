9261f2dd985bd1b7019bbe027317c1d727d4646e
Vsevolod Kukol <sevo@xamarin.com>
2016-07-28 09:21:37 +0200

    [Gtk] Fix WebView scrolling
    WebKitGtk supports native scrolling and should
    not be packed into a Gtk.Viewport.

--- Xwt.Gtk/Xwt.GtkBackend/ScrollViewBackend.cs
+++ Xwt.Gtk/Xwt.GtkBackend/ScrollViewBackend.cs
@@ -74,8 +74,16 @@ namespace Xwt.GtkBackend
 					vp.Add (w);
 					Widget.Child = vp;
 				}
-				else if (w is Gtk.Viewport)
+				#if XWT_GTK3
+				else if (w is Gtk.IScrollable)
 					Widget.Child = w;
+				#else
+				// Gtk2 has no interface for natively scrollable widgets, therefore we manually check
+				// for types that should not be packed into a Viewport.
+				// see: https://developer.gnome.org/gtk2/stable/GtkScrolledWindow.html#gtk-scrolled-window-add-with-viewport
+				else if (w is Gtk.Viewport || w is Gtk.TreeView || w is Gtk.TextView || w is Gtk.Layout || w is WebKit.WebView)
+					Widget.Child = w;
+				#endif
 				else {
 					Gtk.Viewport vp = new Gtk.Viewport ();
 					vp.Show ();
