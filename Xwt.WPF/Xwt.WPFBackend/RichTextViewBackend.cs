fb10edf7315e21b587454dfa28161ae840b7439b
Vsevolod Kukol <sevoku@microsoft.com>
2016-09-16 16:47:28 +0200

    [Wpf] Use FlowDocument API to fill RichTextView

--- Xwt.WPF/Xwt.WPFBackend/RichTextViewBackend.cs
+++ Xwt.WPF/Xwt.WPFBackend/RichTextViewBackend.cs
@@ -26,17 +26,14 @@
 
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Xml;
+using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Documents;
-using System.Windows.Markup;
-using Xwt.Backends;
-
-using Xwt.WPFBackend.Utilities;
 using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Media;
 using System.Windows.Navigation;
+using Xwt.Backends;
+using Xwt.WPFBackend.Utilities;
 
 namespace Xwt.WPFBackend
 {
@@ -131,15 +128,23 @@ namespace Xwt.WPFBackend
 			const int FontSize = 16;
 			const int HeaderIncrement = 8;
 
-			StringBuilder builder;
-			XmlWriter writer;
 			FlowDocument doc;
 
 			public RichTextBuffer ()
 			{
-				builder = new StringBuilder ();
-				writer = XmlWriter.Create (builder, new XmlWriterSettings () { OmitXmlDeclaration = true, NewLineOnAttributes = true, Indent = true, IndentChars = "\t" });
-				writer.WriteStartElement ("FlowDocument", "http://schemas.microsoft.com/winfx/2006/xaml/presentation");
+				doc = new FlowDocument();
+			}
+
+			Stack<InlineCollection> blockStack = new Stack<InlineCollection>();
+
+			bool GetOrCreateInline (out InlineCollection block)
+			{
+				if (blockStack.Count == 0) {
+					block = EmitStartParagraph().Inlines;
+					return true;
+				}
+				block = blockStack.Peek();
+				return false;
 			}
 
 			public void EmitText (string text)
@@ -149,155 +154,199 @@ namespace Xwt.WPFBackend
 
 				var lines = text.Split (new[] { Environment.NewLine }, StringSplitOptions.None);
 				var first = true;
+				InlineCollection block;
+				var inlineCreated = GetOrCreateInline(out block);
 				foreach (var line in lines) {
-					if (!first) {
-						writer.WriteStartElement ("LineBreak");
-						writer.WriteEndElement ();
-					}
-					if (!string.IsNullOrEmpty (line)) {
-						writer.WriteElementString ("Run", line);
-					}
+					if (!first)
+						block.Add(new LineBreak ());
+					if (!string.IsNullOrEmpty (line))
+						block.Add(new Run (line));
 					first = false;
 				}
+				if (inlineCreated)
+					EmitEndParagraph();
 			}
 
 			public void EmitStartHeader (int level)
 			{
-				EmitStartParagraph (0);
-				writer.WriteStartElement ("Span");
-				writer.WriteAttributeString ("FontSize", (FontSize + HeaderIncrement * level).ToString ());
+				var block = EmitStartParagraph().Inlines;
+				var header = new Span() { FontSize = FontSize + HeaderIncrement * level };
+				block.Add(header);
+				blockStack.Push(header.Inlines);
 			}
 
 			public void EmitEndHeader()
 			{
-				writer.WriteEndElement();
+				var span = blockStack.Pop();
 				EmitEndParagraph();
 			}
 
+			List currentList;
+			ListItem currentListItem;
+
 			public void EmitOpenList ()
 			{
-				writer.WriteStartElement ("List");
+				if (currentList != null)
+					throw new InvalidOperationException("A new List can not be added to an existing List.");
+				if (blockStack.Count > 0)
+					throw new InvalidOperationException("A list can not be added to an other block.");
+				currentList = new List();
+				doc.Blocks.Add(currentList);
 			}
 
 			public void EmitOpenBullet ()
 			{
-				writer.WriteStartElement ("ListItem");
-				EmitStartParagraph (0);
+				if (currentList == null)
+					throw new InvalidOperationException("Not inside a List.");
+				var paragraph = new Paragraph();
+				currentListItem  = new ListItem(paragraph);
+				currentList.ListItems.Add(currentListItem);
+				blockStack.Push(paragraph.Inlines);
 			}
 
 			public void EmitCloseBullet ()
 			{
-				writer.WriteEndElement ();
-				writer.WriteEndElement ();
+				if (currentList == null)
+					throw new InvalidOperationException("Not inside a List.");
+				if (currentListItem == null)
+					throw new InvalidOperationException("Not inside a ListItem.");
+				currentListItem = null;
+				blockStack.Pop();
 			}
 
 			public void EmitCloseList ()
 			{
-				// Close the list
-				writer.WriteEndElement ();
+				if (currentList == null)
+					throw new InvalidOperationException("Not inside a List.");
+				if (currentListItem != null)
+					EmitCloseBullet();
+				currentList = null;
 			}
+
+			bool localLinkParagraph;
 
 			public void EmitStartLink (string href, string title)
 			{
-				writer.WriteStartElement ("Hyperlink");
-				writer.WriteAttributeString ("NavigateUri", href);
-				if (!string.IsNullOrEmpty (title))
-					writer.WriteAttributeString ("ToolTip", title);
+				InlineCollection block;
+				localLinkParagraph = GetOrCreateInline(out block);
+				var link = new Hyperlink();
+				link.NavigateUri = new Uri (href);
+				if (!string.IsNullOrEmpty(title))
+					link.ToolTip = title;
+				block.Add(link);
+				blockStack.Push(link.Inlines);
 			}
 
 			public void EmitEndLink()
 			{
-				writer.WriteEndElement();
+				blockStack.Pop();
+				if (localLinkParagraph)
+					EmitEndParagraph();
+				localLinkParagraph = false;
 			}
 
 			int rtbCounter;
 			public void EmitCodeBlock (string code)
 			{
-				writer.WriteStartElement ("BlockUIContainer");
-				writer.WriteAttributeString ("Margin", "15");
+				if (blockStack.Count > 0)
+					throw new InvalidOperationException("A Code Block can not be added to an other block.");
 
-				string name = "rtb" + (rtbCounter++);
+				var rtb = new RichTextBox();
+				rtb.Name = "rtb" + (rtbCounter++);
+				rtb.HorizontalScrollBarVisibility = ScrollBarVisibility.Hidden;
+				var codeDoc = new FlowDocument();
+				var widthBinding = new Binding();
+				widthBinding.Path = new PropertyPath("ActualWidth");
+				widthBinding.ElementName = rtb.Name;
+				codeDoc.SetBinding(FlowDocument.PageWidthProperty, widthBinding);
+				var p = new Paragraph();
+				p.FontFamily = new FontFamily("Global Monospace");
+				p.Inlines.Add(code);
+				codeDoc.Blocks.Add(p);
+				rtb.Document = codeDoc;
 
-				writer.WriteStartElement ("RichTextBox");
-				writer.WriteAttributeString ("Name", name);
-				writer.WriteAttributeString ("HorizontalScrollBarVisibility", "Hidden");
 
-				writer.WriteStartElement ("FlowDocument");
-				//writer.WriteAttributeString ("PageWidth", "1000");
-				writer.WriteAttributeString ("PageWidth", "{Binding ElementName=" + name + ",Path=ActualWidth}");
-				writer.WriteStartElement ("Paragraph");
+				var container = new BlockUIContainer(rtb);
+				container.Margin = new Thickness(15, 15, 15, 15);
 
-				writer.WriteAttributeString ("xml", "space", null, "preserve");
-				writer.WriteAttributeString ("FontFamily", "Global Monospace");
-				writer.WriteString (code);
-
-				writer.WriteEndElement ();
-				writer.WriteEndElement ();
-				writer.WriteEndElement ();
-				writer.WriteEndElement ();
+				doc.Blocks.Add(container);
 			}
 
 			public void EmitText (string text, RichTextInlineStyle style)
 			{
+				InlineCollection block;
+				var inlineCreated = GetOrCreateInline(out block);
+
 				switch (style) {
 				case RichTextInlineStyle.Bold:
+					var bold = new Bold();
+					blockStack.Push(bold.Inlines);
+					EmitText(text);
+					blockStack.Pop();
+					break;
+					
 				case RichTextInlineStyle.Italic:
-					writer.WriteStartElement (style.ToString ());
-					EmitText (text);
+					var italic = new Italic();
+					blockStack.Push(italic.Inlines);
+					EmitText(text);
+					blockStack.Pop();
 					break;
 
 				case RichTextInlineStyle.Monospace:
-					writer.WriteStartElement ("Run");
-					writer.WriteAttributeString ("FontFamily", "Global Monospace");
-					writer.WriteString (text);
+					var run = new Run(text);
+					run.FontFamily = new FontFamily("Global Monospace");
+					block.Add(run);
 					break;
 
 				default:
 					EmitText (text);
 					return;
 				}
-				writer.WriteEndElement ();
+				if (inlineCreated)
+					EmitEndParagraph();
 			}
 
-			public void EmitText (FormattedText text)
+			public void EmitText (FormattedText markup)
 			{
-				EmitText (text.Text, RichTextInlineStyle.Normal);
+				EmitText (markup.Text, RichTextInlineStyle.Normal);
+			}
+
+			Paragraph EmitStartParagraph ()
+			{
+				var paragraph = new Paragraph();
+				blockStack.Push(paragraph.Inlines);
+				doc.Blocks.Add(paragraph);
+				return paragraph;
 			}
 
 			public void EmitStartParagraph (int indentLevel)
 			{
 				//FIXME: indentLevel
-				writer.WriteStartElement ("Paragraph");
+				EmitStartParagraph();
 			}
 
 			public void EmitEndParagraph ()
 			{
-				writer.WriteEndElement ();
+				blockStack.Pop();
 			}
 
 			public void EmitHorizontalRuler ()
 			{
-				writer.WriteStartElement("BlockUIContainer");
-				writer.WriteAttributeString ("Margin", "0,0,0,0");
-				writer.WriteElementString("Separator", "");
-				writer.WriteEndElement();
+				if (blockStack.Count > 0)
+					throw new InvalidOperationException("A Ruler can not be added to an other block.");
+				var ruler = new BlockUIContainer(new System.Windows.Controls.Separator ());
+				ruler.Margin = new Thickness (0, 0, 0, 0);
+				doc.Blocks.Add(ruler);
 			}
 
 			public FlowDocument ToFlowDocument ()
 			{
-				if (doc == null) {
-					writer.WriteEndElement ();
-					writer.Flush ();
-					doc = (FlowDocument) XamlReader.Parse (builder.ToString ());
-					writer = null;
-				}
 				return doc;
 			}
 
 			public string PlainText { 
 				get {
-					var flowDoc = ToFlowDocument();
-					string text = new TextRange (flowDoc.ContentStart, flowDoc.ContentEnd).Text;
+					string text = new TextRange (doc.ContentStart, doc.ContentEnd).Text;
 					return text;
 				}
 			}
