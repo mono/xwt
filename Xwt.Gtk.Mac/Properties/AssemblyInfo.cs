2e4f8c1ecf2bfe7d095e0f5b1a926d12de8c3714
Vsevolod Kukol <sevoku@microsoft.com>
2017-02-11 13:07:45 +0100

    [Gtk] Update assembly metadata of platform specific backends

--- Xwt.Gtk.Mac/Properties/AssemblyInfo.cs
+++ Xwt.Gtk.Mac/Properties/AssemblyInfo.cs
@@ -28,18 +28,19 @@ using System.Runtime.CompilerServices;
 
 // Information about this assembly is defined by the following attributes.
 // Change them to the values specific to your project.
-[assembly: AssemblyTitle ("Xwt.GtkMac")]
-[assembly: AssemblyDescription ("")]
+[assembly: AssemblyTitle ("Xwt.Gtk.Mac")]
+[assembly: AssemblyDescription("Gtk Toolkit for the Xwt UI Framework on Mac")]
 [assembly: AssemblyConfiguration ("")]
-[assembly: AssemblyCompany ("Xamarin")]
-[assembly: AssemblyProduct ("")]
+[assembly: AssemblyCompany ("")]
+[assembly: AssemblyProduct("Xwt UI Framework")]
 [assembly: AssemblyCopyright ("Xamarin, Inc (http://www.xamarin.com)")]
 [assembly: AssemblyTrademark ("")]
 [assembly: AssemblyCulture ("")]
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
-[assembly: AssemblyVersion ("1.0.*")]
+[assembly: AssemblyVersion ("0.1.0.0")]
+[assembly: AssemblyInformationalVersion("0.1.0.0-prerelease")]
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
 //[assembly: AssemblyDelaySign(false)]
