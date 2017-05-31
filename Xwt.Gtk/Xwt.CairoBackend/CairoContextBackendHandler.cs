24871c7bc107d7f40186fa5284e1aed282ca44f8
Marius Ungureanu <teromario@yahoo.com>
2017-05-31 09:03:38 -0700

    Perf opt (#694)
    * [Xwt] Performance improvement on context handling.
    
    Don't use a stack to handle the save/restore, but actually link the items together. This reduces allocations caused by the stacks on every operation to actually one more item allocation.
    Also, normalize handling of save/restore. Do not throw on restore not having an item (this is made to be the same as Restore in Context.cs
    
    * [Perf] Don't allocate lambda closures on drawing images.
    
    * [Mac] CurrentStatus should not be null for the mac backend.
    
    * Fixup! Missing part from previous commit

--- Xwt.Gtk/Xwt.CairoBackend/CairoContextBackendHandler.cs
+++ Xwt.Gtk/Xwt.CairoBackend/CairoContextBackendHandler.cs
@@ -45,11 +45,12 @@ namespace Xwt.CairoBackend
 		public StyleSet Styles;
 		internal Point Origin = Point.Zero;
 
-		Stack<Data> dataStack = new Stack<Data> ();
+		Data stackTop;
 
-		struct Data {
+		class Data {
 			public double PatternAlpha;
 			public double GlobalAlpha;
+			public Data Previous;
 		}
 
 		public CairoContextBackend (double scaleFactor)
@@ -72,16 +73,21 @@ namespace Xwt.CairoBackend
 		public void Save ()
 		{
 			Context.Save ();
-			dataStack.Push (new Data () {
+			stackTop = new Data {
 				PatternAlpha = PatternAlpha,
-				GlobalAlpha = GlobalAlpha
-			});
+				GlobalAlpha = GlobalAlpha,
+				Previous = stackTop,
+			};
 		}
 
 		public void Restore ()
 		{
 			Context.Restore ();
-			var d = dataStack.Pop ();
+			if (stackTop == null)
+				return;
+
+			var d = stackTop;
+			stackTop = stackTop.Previous;
 			PatternAlpha = d.PatternAlpha;
 			GlobalAlpha = d.GlobalAlpha;
 		}
