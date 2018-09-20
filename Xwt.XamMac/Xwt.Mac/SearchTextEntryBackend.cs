c298febe71b2d3c22ca808e4d36eeb6dc8965548
Vsevolod Kukol <sevoku@microsoft.com>
2018-09-20 15:28:14 +0200

    [Mac] TextEntryBackend code improvements

--- Xwt.XamMac/Xwt.Mac/SearchTextEntryBackend.cs
+++ Xwt.XamMac/Xwt.Mac/SearchTextEntryBackend.cs
@@ -40,224 +40,156 @@ namespace Xwt.Mac
 			ViewObject = new CustomAlignedContainer(EventSink, ApplicationContext, view) { DrawsBackground = false, ExpandVertically = true };
 			base.Initialize();
 		}
+	}
 
-		class CustomSearchTextField : NSSearchField, IViewObject
+	class CustomSearchTextField : NSSearchField, IViewObject
+	{
+		ITextEntryEventSink eventSink;
+		ApplicationContext context;
+		#pragma warning disable CS0414 // The private field is assigned but its value is never used
+		CustomCell cell;
+		#pragma warning disable CS0414
+
+		public CustomSearchTextField(ITextEntryEventSink eventSink, ApplicationContext context)
 		{
-			ITextEntryEventSink eventSink;
-			ApplicationContext context;
-			#pragma warning disable CS0414 // The private field is assigned but its value is never used
-			CustomCell cell;
-			#pragma warning disable CS0414
-
-			public CustomSearchTextField(ITextEntryEventSink eventSink, ApplicationContext context)
+			this.context = context;
+			this.eventSink = eventSink;
+			this.Cell = cell = new CustomCell
 			{
-				this.context = context;
-				this.eventSink = eventSink;
-				this.Cell = cell = new CustomCell
-				{
-					Bezeled = true,
-					Editable = true,
-					EventSink = eventSink,
-					Context = context,
-				};
+				Bezeled = true,
+				Editable = true,
+				EventSink = eventSink,
+				Context = context,
+			};
+		}
+
+		public NSView View
+		{
+			get
+			{
+				return this;
 			}
+		}
 
-			public NSView View
+		public ViewBackend Backend { get; set; }
+
+		public override void DidChange(NSNotification notification)
+		{
+			base.DidChange(notification);
+			context.InvokeUserCode(delegate
 			{
-				get
-				{
-					return this;
-				}
-			}
+				eventSink.OnChanged();
+				eventSink.OnSelectionChanged();
+			});
+		}
 
-			public ViewBackend Backend { get; set; }
-
-			public override void DidChange(NSNotification notification)
+		public override string StringValue
+		{
+			get { return base.StringValue; }
+			set
 			{
-				base.DidChange(notification);
-				context.InvokeUserCode(delegate
+				if (base.StringValue != value)
 				{
-					eventSink.OnChanged();
-					eventSink.OnSelectionChanged();
-				});
-			}
-
-			public override string StringValue
-			{
-				get { return base.StringValue; }
-				set
-				{
-					if (base.StringValue != value)
+					base.StringValue = value;
+					context.InvokeUserCode(delegate
 					{
-						base.StringValue = value;
-						context.InvokeUserCode(delegate
-						{
-							eventSink.OnChanged();
-							eventSink.OnSelectionChanged();
-						});
-					}
-				}
-			}
-
-			class CustomCell : NSSearchFieldCell
-			{
-				CustomEditor editor;
-				NSObject selChangeObserver;
-				public ApplicationContext Context
-				{
-					get; set;
-				}
-
-				public ITextEntryEventSink EventSink
-				{
-					get; set;
-				}
-
-				public CustomCell()
-				{
-
-				}
-
-				protected CustomCell(IntPtr ptr) : base(ptr)
-				{
-				}
-
-				/// <summary>
-				/// Like what happens for the ios designer, AppKit can sometimes clone the native `NSTextFieldCell` using the Copy (NSZone)
-				/// method. We *need* to ensure we can create a new managed wrapper for the cloned native object so we need the IntPtr
-				/// constructor. NOTE: By keeping this override in managed we ensure the new wrapper C# object is created ~immediately,
-				/// which makes it easier to debug issues.
-				/// </summary>
-				/// <returns>The copy.</returns>
-				/// <param name="zone">Zone.</param>
-				public override NSObject Copy(NSZone zone)
-				{
-					// Don't remove this override because the comment on this explains why we need this!
-					var newCell = (CustomCell)base.Copy(zone);
-					newCell.editor = editor;
-					newCell.selChangeObserver = selChangeObserver;
-					newCell.Context = Context;
-					newCell.EventSink = EventSink;
-					return newCell;
-				}
-
-				public override NSTextView FieldEditorForView(NSView aControlView)
-				{
-					if (editor == null)
-					{
-						editor = new CustomEditor
-						{
-							Context = this.Context,
-							EventSink = this.EventSink,
-							FieldEditor = true,
-							Editable = true,
-						};
-						selChangeObserver = NSNotificationCenter.DefaultCenter.AddObserver(new NSString("NSTextViewDidChangeSelectionNotification"), HandleSelectionDidChange, editor);
-					}
-					return editor;
-				}
-
-				void HandleSelectionDidChange(NSNotification notif)
-				{
-					Context.InvokeUserCode(EventSink.OnSelectionChanged);
-				}
-
-				public override void DrawInteriorWithFrame(CGRect cellFrame, NSView inView)
-				{
-					base.DrawInteriorWithFrame(VerticalCenteredRectForBounds(cellFrame), inView);
-				}
-
-				public override void EditWithFrame(CGRect aRect, NSView inView, NSText editor, NSObject delegateObject, NSEvent theEvent)
-				{
-					base.EditWithFrame(VerticalCenteredRectForBounds(aRect), inView, editor, delegateObject, theEvent);
-				}
-
-				public override void SelectWithFrame(CGRect aRect, NSView inView, NSText editor, NSObject delegateObject, nint selStart, nint selLength)
-				{
-					base.SelectWithFrame(VerticalCenteredRectForBounds(aRect), inView, editor, delegateObject, selStart, selLength);
-				}
-
-				CGRect VerticalCenteredRectForBounds(CGRect aRect)
-				{
-					// multiline entries should always align on top
-					if (!UsesSingleLineMode)
-						return aRect;
-
-					var textHeight = CellSizeForBounds(aRect).Height;
-					var offset = (aRect.Height - textHeight) / 2;
-					if (offset <= 0) // do nothing if the frame is too small
-						return aRect;
-					var rect = new Rectangle(aRect.X, aRect.Y, aRect.Width, aRect.Height).Inflate(0.0, -offset);
-					return rect.ToCGRect();
-				}
-			}
-
-			class CustomEditor : NSTextView
-			{
-				public ApplicationContext Context
-				{
-					get; set;
-				}
-
-				public ITextEntryEventSink EventSink
-				{
-					get; set;
-				}
-
-				public CustomEditor()
-				{
-
-				}
-
-				public override void KeyDown(NSEvent theEvent)
-				{
-					Context.InvokeUserCode(delegate
-					{
-						EventSink.OnKeyPressed(theEvent.ToXwtKeyEventArgs());
+						eventSink.OnChanged();
+						eventSink.OnSelectionChanged();
 					});
-					base.KeyDown(theEvent);
 				}
+			}
+		}
 
-				nint cachedCursorPosition;
-				public override void KeyUp(NSEvent theEvent)
-				{
-					if (cachedCursorPosition != SelectedRange.Location)
-					{
-						cachedCursorPosition = SelectedRange.Location;
-						Context.InvokeUserCode(delegate
-						{
-							EventSink.OnSelectionChanged();
-							EventSink.OnKeyReleased(theEvent.ToXwtKeyEventArgs());
-						});
-					}
-					base.KeyUp(theEvent);
-				}
+		class CustomCell : NSSearchFieldCell
+		{
+			NSTextView editor;
+			NSObject selChangeObserver;
+			public ApplicationContext Context
+			{
+				get; set;
+			}
 
-				public override bool BecomeFirstResponder()
-				{
-					var result = base.BecomeFirstResponder();
-					if (result)
-					{
-						Context.InvokeUserCode(() =>
-						{
-							EventSink.OnGotFocus();
-						});
-					}
-					return result;
-				}
+			public ITextEntryEventSink EventSink
+			{
+				get; set;
+			}
 
-				public override bool ResignFirstResponder()
+			public CustomCell()
+			{
+
+			}
+
+			protected CustomCell(IntPtr ptr) : base(ptr)
+			{
+			}
+
+			/// <summary>
+			/// Like what happens for the ios designer, AppKit can sometimes clone the native `NSTextFieldCell` using the Copy (NSZone)
+			/// method. We *need* to ensure we can create a new managed wrapper for the cloned native object so we need the IntPtr
+			/// constructor. NOTE: By keeping this override in managed we ensure the new wrapper C# object is created ~immediately,
+			/// which makes it easier to debug issues.
+			/// </summary>
+			/// <returns>The copy.</returns>
+			/// <param name="zone">Zone.</param>
+			public override NSObject Copy(NSZone zone)
+			{
+				// Don't remove this override because the comment on this explains why we need this!
+				var newCell = (CustomCell)base.Copy(zone);
+				newCell.editor = editor;
+				newCell.selChangeObserver = selChangeObserver;
+				newCell.Context = Context;
+				newCell.EventSink = EventSink;
+				return newCell;
+			}
+
+			public override NSTextView FieldEditorForView(NSView aControlView)
+			{
+				if (editor == null)
 				{
-					var result = base.ResignFirstResponder();
-					if (result)
+					editor = new CustomTextFieldCellEditor
 					{
-						Context.InvokeUserCode(() =>
-						{
-							EventSink.OnLostFocus();
-						});
-					}
-					return result;
+						Context = this.Context,
+						EventSink = this.EventSink,
+						FieldEditor = true,
+						Editable = true,
+					};
+					selChangeObserver = NSNotificationCenter.DefaultCenter.AddObserver(new NSString("NSTextViewDidChangeSelectionNotification"), HandleSelectionDidChange, editor);
 				}
+				return editor;
+			}
+
+			void HandleSelectionDidChange(NSNotification notif)
+			{
+				Context.InvokeUserCode(EventSink.OnSelectionChanged);
+			}
+
+			public override void DrawInteriorWithFrame(CGRect cellFrame, NSView inView)
+			{
+				base.DrawInteriorWithFrame(VerticalCenteredRectForBounds(cellFrame), inView);
+			}
+
+			public override void EditWithFrame(CGRect aRect, NSView inView, NSText editor, NSObject delegateObject, NSEvent theEvent)
+			{
+				base.EditWithFrame(VerticalCenteredRectForBounds(aRect), inView, editor, delegateObject, theEvent);
+			}
+
+			public override void SelectWithFrame(CGRect aRect, NSView inView, NSText editor, NSObject delegateObject, nint selStart, nint selLength)
+			{
+				base.SelectWithFrame(VerticalCenteredRectForBounds(aRect), inView, editor, delegateObject, selStart, selLength);
+			}
+
+			CGRect VerticalCenteredRectForBounds(CGRect aRect)
+			{
+				// multiline entries should always align on top
+				if (!UsesSingleLineMode)
+					return aRect;
+
+				var textHeight = CellSizeForBounds(aRect).Height;
+				var offset = (aRect.Height - textHeight) / 2;
+				if (offset <= 0) // do nothing if the frame is too small
+					return aRect;
+				var rect = new Rectangle(aRect.X, aRect.Y, aRect.Width, aRect.Height).Inflate(0.0, -offset);
+				return rect.ToCGRect();
 			}
 		}
 	}
