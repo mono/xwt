41f862750d39f4f39640ac056b998acae5e5ac1b
Jose Medrano <jose.medrano@microsoft.com>
2019-04-11 09:25:27 +0200

    Adds generic Control, Window and derived subclasses

---
+++ Xwt.Components/Control.cs
@@ -0,0 +1,210 @@
+//
+// Control.cs
+//
+// Author:
+//       Lluis Sanchez Gual <lluis@xamarin.com>
+//
+// Copyright (c) 2014 Xamarin, Inc (http://www.xamarin.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+#if MAC
+using AppKit;
+#endif
+
+namespace Xwt.Components
+{
+	public class Control : IDisposable
+	{
+		internal static ConditionalWeakTable<object, Control> cache = new ConditionalWeakTable<object, Control> ();
+		internal object nativeWidget; // TODO: This should be a weak reference, not a strong reference, if we're not passing ownership
+
+		protected Control ()
+		{
+		}
+
+		protected Control (object widget)
+		{
+			nativeWidget = widget ?? throw new ArgumentNullException (nameof (widget));
+			cache.Add (nativeWidget, this);
+		}
+
+		protected virtual object CreateNativeWidget<T> ()
+		{
+			throw new NotSupportedException ();
+		}
+
+		public virtual T GetNativeWidget<T> ()
+		{
+			if (nativeWidget == null) {
+				var toCache = this;
+				var w = CreateNativeWidget<T> ();
+				if (!(w is T)) {
+					var temp = w as Control;
+					while (temp != null) {
+						w = temp.GetNativeWidget<T> ();
+						temp = w as Control;
+					}
+					w = ConvertToType (typeof (T), w);
+				}
+				nativeWidget = w;
+				if (cache.TryGetValue (nativeWidget, out Control target)) {
+					if (target != toCache)
+						throw new InvalidOperationException ($"Widget {nativeWidget.GetType ()} has been mapped to multiple controls");
+				} else
+					cache.Add (nativeWidget, toCache);
+			}
+			if (nativeWidget is T resultWidget)
+				return resultWidget;
+			else
+				throw new NotSupportedException ();
+		}
+
+		void OnGtkDestroyed (object sender, EventArgs args)
+		{
+			GC.SuppressFinalize (this);
+			Dispose (true);
+		}
+
+		static object ConvertToType (Type t, object w)
+		{
+			if (t.IsInstanceOfType (w))
+				return w;
+
+#if MAC
+			if (w is NSView && t == typeof (Gtk.Widget)) {
+				var ww = GtkMacInterop.NSViewToGtkWidget ((NSView)w);
+				ww.Show ();
+				return ww;
+			}
+			if (w is Gtk.Widget && t == typeof (NSView)) {
+				return new GtkEmbed ((Gtk.Widget)w);
+			}
+#endif
+			throw new NotSupportedException ();
+		}
+
+#if MAC
+		public static implicit operator NSView (Control d)
+		{
+			return d.GetNativeWidget<NSView> ();
+		}
+
+		public static implicit operator Control (NSView d)
+		{
+			if (d == null)
+				return null;
+
+			return GetImplicit<Control, NSView> (d) ?? new Control (d);
+		}
+#endif
+
+		public static implicit operator Gtk.Widget (Control d)
+		{
+			return d?.GetNativeWidget<Gtk.Widget> ();
+		}
+
+		public static implicit operator Control (Gtk.Widget d)
+		{
+			if (d == null)
+				return null;
+
+			var control = GetImplicit<Control, Gtk.Widget> (d);
+			if (control == null) {
+				control = new Control (d);
+				d.Destroyed += delegate {
+					GC.SuppressFinalize (control);
+					control.Dispose (true);
+				};
+			}
+			return control;
+		}
+
+		public static implicit operator Xwt.Widget (Control d)
+		{
+			if (d is AbstractXwtControl)
+				return ((AbstractXwtControl)d).Widget;
+
+			object nativeWidget;
+			if (Xwt.Toolkit.CurrentEngine.Type == Xwt.ToolkitType.Gtk && (nativeWidget = d?.GetNativeWidget<Gtk.Widget> ()) != null) {
+				return Xwt.Toolkit.CurrentEngine.WrapWidget (nativeWidget, Xwt.NativeWidgetSizing.DefaultPreferredSize);
+			}
+#if MAC
+			else if (Xwt.Toolkit.CurrentEngine.Type == Xwt.ToolkitType.XamMac && (nativeWidget = d?.GetNativeWidget<NSView> ()) != null) {
+				return Xwt.Toolkit.CurrentEngine.WrapWidget (nativeWidget, Xwt.NativeWidgetSizing.DefaultPreferredSize);
+			}
+#endif
+			throw new NotSupportedException ();
+		}
+
+		internal static T GetImplicit<T, U> (U native) where T : Control where U : class
+		{
+			if (cache.TryGetValue (native, out Control target) && target is T ret) {
+				return ret;
+			}
+			return null;
+		}
+
+		public virtual void GrabFocus ()
+		{
+			if (nativeWidget is Gtk.Widget)
+				((Gtk.Widget)nativeWidget).GrabFocus ();
+			// TODO
+		}
+
+
+		public virtual bool HasFocus {
+			get {
+				// TODO
+				if (nativeWidget is Gtk.Widget)
+					return ((Gtk.Widget)nativeWidget).HasFocus;
+				return false;
+			}
+		}
+
+		public void Dispose ()
+		{
+			var gtkWidget = nativeWidget as Gtk.Widget;
+			if (gtkWidget != null) {
+				gtkWidget.Destroy ();
+			}
+#if MAC
+			else if (nativeWidget is NSView)
+				((NSView)nativeWidget).Dispose ();
+#endif
+
+			Dispose (true);
+		}
+
+		protected virtual void Dispose (bool disposing)
+		{
+			if (nativeWidget != null)
+				cache.Remove (nativeWidget);
+
+			var gtkWidget = nativeWidget as Gtk.Widget;
+			if (gtkWidget != null) {
+				gtkWidget.Destroyed -= OnGtkDestroyed;
+			}
+		}
+	}
+}
+
---
+++ Xwt.Components/GtkUtil.cs
@@ -0,0 +1,99 @@
+﻿//
+// GtkMacInterop.cs
+//
+// Author:
+//       Jérémie Laval <jeremie.laval@xamarin.com>
+//
+// Copyright (c) 2012 Xamarin, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using System;
+using System.Runtime.InteropServices;
+using Gdk;
+
+namespace Xwt.Components
+{
+	internal class GtkUtil
+	{
+		class EventKeyWrapper : Gdk.EventKey
+		{
+			IntPtr ptr;
+
+			public EventKeyWrapper (IntPtr ptr) : base (ptr)
+			{
+				this.ptr = ptr;
+			}
+
+			~EventKeyWrapper ()
+			{
+				Marshal.FreeHGlobal (ptr);
+			}
+		}
+
+		// Analysis disable InconsistentNaming
+		[StructLayout (LayoutKind.Sequential)]
+		struct NativeEventKeyStruct
+		{
+			public Gdk.EventType type;
+			public IntPtr window;
+			public sbyte send_event;
+			public uint time;
+			public uint state;
+			public uint keyval;
+			public int length;
+			public IntPtr str;
+			public ushort hardware_keycode;
+			public byte group;
+			public uint is_modifier;
+		}
+
+		public static Gdk.EventKey CreateKeyEventFromKeyCode (ushort keyCode, Gdk.ModifierType state, Gdk.EventType eventType, Gdk.Window win, uint time)
+		{
+			return CreateKeyEvent (0, keyCode, state, eventType, win, time);
+		}
+
+		static Gdk.EventKey CreateKeyEvent (uint keyval, int keyCode, Gdk.ModifierType state, Gdk.EventType eventType, Gdk.Window win, uint? time)
+		{
+			int effectiveGroup, level;
+			Gdk.ModifierType cmods;
+			if (keyval == 0)
+				Gdk.Keymap.Default.TranslateKeyboardState ((uint)keyCode, state, 0, out keyval, out effectiveGroup, out level, out cmods);
+
+			Gdk.KeymapKey [] keyms = Gdk.Keymap.Default.GetEntriesForKeyval (keyval);
+			if (keyms.Length == 0)
+				return null;
+
+			var nativeEvent = new NativeEventKeyStruct {
+				type = eventType,
+				send_event = 1,
+				window = win != null ? win.Handle : IntPtr.Zero,
+				state = (uint)state,
+				keyval = keyval,
+				group = (byte)keyms [0].Group,
+				hardware_keycode = keyCode == -1 ? (ushort)keyms [0].Keycode : (ushort)keyCode,
+				length = 0,
+				time = time ?? Gtk.Global.CurrentEventTime
+			};
+
+			IntPtr ptr = GLib.Marshaller.StructureToPtrAlloc (nativeEvent);
+			return new EventKeyWrapper (ptr);
+		}
+	}
+}
