mono/xwt

commit 2102683398a84ee16094fc4bcae3dac44ba31872

Add XamMac and Gtk backends

Xwt.Gtk/Xwt.GtkBackend/LabelBackend.cs
@@ -163,18 +163,7 @@ namespace Xwt.GtkBackend
 		{
 			Label.Text = text.Text;
 			formattedText = text;
-			var list = new FastPangoAttrList ();
-			if (Label.IsRealized) {
-				var color = Gdk.Color.Zero;
-				var colorVal = Label.StyleGetProperty ("link-color");
-				if (colorVal is Gdk.Color)
-					color = (Gdk.Color)colorVal;
-				if (!color.Equals (Gdk.Color.Zero))
-					list.DefaultLinkColor = color;
-			}
-			indexer = new TextIndexer (text.Text);
-			list.AddAttributes (indexer, text.Attributes);
-			gtk_label_set_attributes (Label.Handle, list.Handle);
+			text.ApplyToLabel (Label);
 
 			if (links != null)
 				links.Clear ();

Xwt.Gtk/Xwt.GtkBackend/Util.cs
@@ -30,6 +30,7 @@ using Xwt.Drawing;
 using Xwt.Backends;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 
 namespace Xwt.GtkBackend
 {
@@ -275,6 +276,26 @@ namespace Xwt.GtkBackend
 		internal static void Dispose (this Cairo.Context cr)
 		{
 			((IDisposable)cr).Dispose ();
+		}
+
+		[DllImport (GtkInterop.LIBGTK, CallingConvention = CallingConvention.Cdecl)]
+		static extern void gtk_label_set_attributes (IntPtr label, IntPtr attrList);
+
+		internal static void ApplyToLabel(this FormattedText text, Gtk.Label label)
+		{
+			var list = new FastPangoAttrList ();
+			if (label.IsRealized) {
+				var color = Gdk.Color.Zero;
+				var colorVal = label.StyleGetProperty ("link-color");
+				if (colorVal is Gdk.Color)
+					color = (Gdk.Color)colorVal;
+				if (!color.Equals (Gdk.Color.Zero))
+					list.DefaultLinkColor = color;
+			}
+			var indexer = new TextIndexer (text.Text);
+			list.AddAttributes (indexer, text.Attributes);
+			gtk_label_set_attributes (label.Handle, list.Handle);
+
 		}
 	}
 }

Xwt.WPF/Xwt.WPFBackend/LabelBackend.cs
@@ -110,80 +110,7 @@ namespace Xwt.WPFBackend
 
 		public void SetFormattedText (FormattedText text)
 		{
-			var atts = new List<Drawing.TextAttribute> (text.Attributes);
-			atts.Sort ((a, b) => {
-				var c = a.StartIndex.CompareTo (b.StartIndex);
-				if (c == 0)
-					c = -(a.Count.CompareTo (b.Count));
-				return c;
-			});
-
-			int i = 0, attrIndex = 0;
-			Label.TextBlock.Inlines.Clear ();
-			GenerateBlocks (Label.TextBlock.Inlines, text.Text, ref i, text.Text.Length, atts, ref attrIndex);
-		}
-
-		void GenerateBlocks (SWD.InlineCollection col, string text, ref int i, int spanEnd, List<Drawing.TextAttribute> attributes, ref int attrIndex)
-		{
-			while (attrIndex < attributes.Count) {
-				var at = attributes[attrIndex];
-				if (at.StartIndex > spanEnd) {
-					FlushText (col, text, ref i, spanEnd);
-					return;
-				}
-
-				FlushText (col, text, ref i, at.StartIndex);
-
-				var s = new SWD.Span ();
-
-				if (at is Drawing.BackgroundTextAttribute) {
-					s.Background = new SWM.SolidColorBrush (((Drawing.BackgroundTextAttribute)at).Color.ToWpfColor ());
-				}
-				else if (at is Drawing.FontWeightTextAttribute) {
-					s.FontWeight = ((Drawing.FontWeightTextAttribute)at).Weight.ToWpfFontWeight ();
-				}
-				else if (at is Drawing.FontStyleTextAttribute) {
-					s.FontStyle = ((Drawing.FontStyleTextAttribute)at).Style.ToWpfFontStyle ();
-				}
-				else if (at is Drawing.UnderlineTextAttribute) {
-					var xa = (Drawing.UnderlineTextAttribute)at;
-					var dec = new TextDecoration (TextDecorationLocation.Underline, null, 0, TextDecorationUnit.FontRecommended, TextDecorationUnit.FontRecommended);
-					s.TextDecorations.Add (dec);
-				}
-				else if (at is Drawing.StrikethroughTextAttribute) {
-					var xa = (Drawing.StrikethroughTextAttribute)at;
-					var dec = new TextDecoration (TextDecorationLocation.Strikethrough, null, 0, TextDecorationUnit.FontRecommended, TextDecorationUnit.FontRecommended);
-					s.TextDecorations.Add (dec);
-				}
-				else if (at is Drawing.FontTextAttribute) {
-					var xa = (Drawing.FontTextAttribute)at;
-					s.FontFamily = new SWM.FontFamily (xa.Font.Family);
-					s.FontSize = WpfFontBackendHandler.GetPointsFromDeviceUnits (xa.Font.Size);
-					s.FontStretch = xa.Font.Stretch.ToWpfFontStretch ();
-					s.FontStyle = xa.Font.Style.ToWpfFontStyle ();
-					s.FontWeight = xa.Font.Weight.ToWpfFontWeight ();
-				}
-				else if (at is Drawing.ColorTextAttribute) {
-					s.Foreground = new SWM.SolidColorBrush (((Drawing.ColorTextAttribute)at).Color.ToWpfColor ());
-				}
-				else if (at is Drawing.LinkTextAttribute) {
-					var link = new SWD.Hyperlink () {
-						NavigateUri = ((Drawing.LinkTextAttribute)at).Target
-					};
-					link.RequestNavigate += new System.Windows.Navigation.RequestNavigateEventHandler (link_RequestNavigate);
-					s = link;
-				}
-
-				col.Add (s);
-
-				var max = i + at.Count;
-				if (max > spanEnd)
-					max = spanEnd;
-
-				attrIndex++;
-				GenerateBlocks (s.Inlines, text, ref i, i + at.Count, attributes, ref attrIndex);
-			}
-			FlushText (col, text, ref i, spanEnd);
+			Label.TextBlock.ApplyFormattedText (text, link_RequestNavigate);
 		}
 
 		void link_RequestNavigate (object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
@@ -191,14 +118,6 @@ namespace Xwt.WPFBackend
 			Context.InvokeUserCode (delegate {
 				EventSink.OnLinkClicked (e.Uri);
 			});
-		}
-
-		void FlushText (SWD.InlineCollection col, string text, ref int i, int pos)
-		{
-			if (pos > i) {
-				col.Add (text.Substring (i, pos - i));
-				i = pos;
-			}
 		}
 
 		public Xwt.Drawing.Color TextColor {

Xwt.WPF/Xwt.WPFBackend/Util.cs
@@ -24,9 +24,13 @@
 // THE SOFTWARE.
 
 using System.Windows;
-using System.Windows.Media;
+using SWC = System.Windows.Controls;
+using SWM = System.Windows.Media;
+using SWD = System.Windows.Documents;
+
 using System;
 using Xwt.Backends;
+using System.Collections.Generic;
 
 namespace Xwt.WPFBackend
 {
@@ -104,6 +108,87 @@ namespace Xwt.WPFBackend
 				return parentWindow;
 
 			return System.Windows.Application.Current.MainWindow;
+		}
+
+		internal static void ApplyFormattedText (this SWC.TextBlock textBlock, FormattedText text, System.Windows.Navigation.RequestNavigateEventHandler link_RequestNavigate)
+		{
+			var atts = new List<Drawing.TextAttribute> (text.Attributes);
+			atts.Sort ((a, b) => {
+				var c = a.StartIndex.CompareTo (b.StartIndex);
+				if (c == 0)
+					c = -(a.Count.CompareTo (b.Count));
+				return c;
+			});
+
+			int i = 0, attrIndex = 0;
+			textBlock.Inlines.Clear ();
+
+			GenerateBlocks (textBlock.Inlines, text.Text, ref i, text.Text.Length, atts, ref attrIndex, link_RequestNavigate);
+		}
+
+		internal static void GenerateBlocks (SWD.InlineCollection col, string text, ref int i, int spanEnd, List<Drawing.TextAttribute> attributes, ref int attrIndex, System.Windows.Navigation.RequestNavigateEventHandler link_RequestNavigate)
+		{
+			while (attrIndex < attributes.Count) {
+				var at = attributes[attrIndex];
+				if (at.StartIndex > spanEnd) {
+					FlushText (col, text, ref i, spanEnd);
+					return;
+				}
+
+				FlushText (col, text, ref i, at.StartIndex);
+
+				var s = new SWD.Span ();
+
+				if (at is Drawing.BackgroundTextAttribute) {
+					s.Background = new SWM.SolidColorBrush (((Drawing.BackgroundTextAttribute)at).Color.ToWpfColor ());
+				} else if (at is Drawing.FontWeightTextAttribute) {
+					s.FontWeight = ((Drawing.FontWeightTextAttribute)at).Weight.ToWpfFontWeight ();
+				} else if (at is Drawing.FontStyleTextAttribute) {
+					s.FontStyle = ((Drawing.FontStyleTextAttribute)at).Style.ToWpfFontStyle ();
+				} else if (at is Drawing.UnderlineTextAttribute) {
+					var xa = (Drawing.UnderlineTextAttribute)at;
+					var dec = new TextDecoration (TextDecorationLocation.Underline, null, 0, TextDecorationUnit.FontRecommended, TextDecorationUnit.FontRecommended);
+					s.TextDecorations.Add (dec);
+				} else if (at is Drawing.StrikethroughTextAttribute) {
+					var xa = (Drawing.StrikethroughTextAttribute)at;
+					var dec = new TextDecoration (TextDecorationLocation.Strikethrough, null, 0, TextDecorationUnit.FontRecommended, TextDecorationUnit.FontRecommended);
+					s.TextDecorations.Add (dec);
+				} else if (at is Drawing.FontTextAttribute) {
+					var xa = (Drawing.FontTextAttribute)at;
+					s.FontFamily = new SWM.FontFamily (xa.Font.Family);
+					s.FontSize = WpfFontBackendHandler.GetPointsFromDeviceUnits (xa.Font.Size);
+					s.FontStretch = xa.Font.Stretch.ToWpfFontStretch ();
+					s.FontStyle = xa.Font.Style.ToWpfFontStyle ();
+					s.FontWeight = xa.Font.Weight.ToWpfFontWeight ();
+				} else if (at is Drawing.ColorTextAttribute) {
+					s.Foreground = new SWM.SolidColorBrush (((Drawing.ColorTextAttribute)at).Color.ToWpfColor ());
+				} else if (at is Drawing.LinkTextAttribute) {
+					var link = new SWD.Hyperlink () {
+						NavigateUri = ((Drawing.LinkTextAttribute)at).Target
+					};
+					link.RequestNavigate += link_RequestNavigate;
+					s = link;
+				}
+
+				col.Add (s);
+
+				var max = i + at.Count;
+				if (max > spanEnd)
+					max = spanEnd;
+
+				attrIndex++;
+				GenerateBlocks (s.Inlines, text, ref i, i + at.Count, attributes, ref attrIndex);
+			}
+			FlushText (col, text, ref i, spanEnd);
+		}
+		
+
+		internal static void FlushText (System.Windows.Drawing.InlineCollection col, string text, ref int i, int pos)
+		{
+			if (pos > i) {
+				col.Add (text.Substring (i, pos - i));
+				i = pos;
+			}
 		}
 	}
 

Xwt.Gtk/Xwt.GtkBackend/ButtonBackend.cs
@@ -220,7 +220,15 @@ namespace Xwt.GtkBackend
 			Button b = (Button) Frontend;
 			SetContent (b.Label, b.UseMnemonic, image, b.ImagePosition);
 		}
-		
+
+		public void SetFormattedText (FormattedText text)
+		{
+			if (labelWidget != null) {
+				labelWidget.Text = text.Text;
+				text.ApplyToLabel (labelWidget);
+			}
+		}
+
 		public override void EnableEvent (object eventId)
 		{
 			base.EnableEvent (eventId);

Xwt.Gtk/Xwt.GtkBackend/MenuItemBackend.cs
@@ -50,6 +50,11 @@ namespace Xwt.GtkBackend
 		{
 			this.item = item;
 			label = (Gtk.Label) item.Child;
+			if (label != null) {
+				label.Realized += HandleStyleUpdate;
+				label.StyleSet += HandleStyleUpdate;
+			}
+
 			item.ShowAll ();
 		}
 		
@@ -168,56 +173,72 @@ namespace Xwt.GtkBackend
 				}
 			}
 		}
-		
-/*		public void SetType (MenuItemType type)
+
+		FormattedText formattedText = null;
+		public void SetFormattedText (FormattedText text)
 		{
-			string text = label.Text;
-			
-			Gtk.MenuItem newItem = null;
-			switch (type) {
-			case MenuItemType.Normal:
-				if (!(item is Gtk.ImageMenuItem))
-					newItem = new Gtk.ImageMenuItem (text);
-				break;
-			case MenuItemType.CheckBox:
-				if (item.GetType () != typeof(Gtk.CheckMenuItem))
-					newItem = new Gtk.CheckMenuItem (text);
-				break;
-			case MenuItemType.RadioButton:
-				if (!(item is Gtk.RadioMenuItem))
-					newItem = new Gtk.RadioMenuItem (text);
-				break;
+			label.Text = text.Text;
+			formattedText = text;
+			text.ApplyToLabel(label);
+		}
+
+		void HandleStyleUpdate (object sender, EventArgs e)
+		{
+			// force text update with updated link color
+			if (label.IsRealized && formattedText != null) {
+				SetFormattedText (formattedText);
 			}
-			
-			if (newItem != null) {
-				if ((newItem is Gtk.CheckMenuItem) && (item is Gtk.CheckMenuItem))
-					((Gtk.CheckMenuItem)item).Active = ((Gtk.CheckMenuItem)newItem).Active;
-				newItem.Sensitive = item.Sensitive;
-				if (item.Parent != null) {
-					Gtk.Menu m = (Gtk.Menu)item.Parent;
-					int pos = Array.IndexOf (m.Children, item);
-					m.Insert (newItem, pos);
-					m.Remove (item);
-				}
-				newItem.ShowAll ();
-				if (!item.Visible)
-					newItem.Hide ();
-				
-				if (enabledEvents != null) {
-					foreach (var ob in enabledEvents)
-						DisableEvent (ob);
-				}
-				
-				item = newItem;
-				label = (Gtk.Label) item.Child;
-				
-				if (enabledEvents != null) {
-					foreach (var ob in enabledEvents)
-						EnableEvent (ob);
-				}
-			}
-		}*/
-		
+		}
+
+		/*		public void SetType (MenuItemType type)
+				{
+					string text = label.Text;
+
+					Gtk.MenuItem newItem = null;
+					switch (type) {
+					case MenuItemType.Normal:
+						if (!(item is Gtk.ImageMenuItem))
+							newItem = new Gtk.ImageMenuItem (text);
+						break;
+					case MenuItemType.CheckBox:
+						if (item.GetType () != typeof(Gtk.CheckMenuItem))
+							newItem = new Gtk.CheckMenuItem (text);
+						break;
+					case MenuItemType.RadioButton:
+						if (!(item is Gtk.RadioMenuItem))
+							newItem = new Gtk.RadioMenuItem (text);
+						break;
+					}
+
+					if (newItem != null) {
+						if ((newItem is Gtk.CheckMenuItem) && (item is Gtk.CheckMenuItem))
+							((Gtk.CheckMenuItem)item).Active = ((Gtk.CheckMenuItem)newItem).Active;
+						newItem.Sensitive = item.Sensitive;
+						if (item.Parent != null) {
+							Gtk.Menu m = (Gtk.Menu)item.Parent;
+							int pos = Array.IndexOf (m.Children, item);
+							m.Insert (newItem, pos);
+							m.Remove (item);
+						}
+						newItem.ShowAll ();
+						if (!item.Visible)
+							newItem.Hide ();
+
+						if (enabledEvents != null) {
+							foreach (var ob in enabledEvents)
+								DisableEvent (ob);
+						}
+
+						item = newItem;
+						label = (Gtk.Label) item.Child;
+
+						if (enabledEvents != null) {
+							foreach (var ob in enabledEvents)
+								EnableEvent (ob);
+						}
+					}
+				}*/
+
 		public void InitializeBackend (object frontend, ApplicationContext context)
 		{
 			this.context = context;

Xwt.WPF/Xwt.WPFBackend/ButtonBackend.cs
@@ -142,6 +142,20 @@ namespace Xwt.WPFBackend
 			Button.InvalidateMeasure ();
 		}
 
+		public void SetFormattedText (FormattedText text)
+		{
+			SWC.Label labelCtrl = null;
+			if (Button.Content is SWC.DockPanel) {
+				var grid = Button.Content as SWC.DockPanel;
+				labelCtrl = grid.Children[1] as SWC.Label;
+				labelCtrl.TextBlock.ApplyFormattedText (text, null);
+			} else {
+				labelCtrl = new SWC.Label ();
+				labelCtrl.TextBlock.ApplyFormattedText (text, null);
+				Button.Content = labelCtrl;
+			}
+		}
+
 		public Xwt.Drawing.Color LabelColor {
 			get { return Button.Foreground.ToXwtColor (); }
 			set { Button.Foreground = ResPool.GetSolidBrush (value.ToWpfColor ()); }

Xwt.XamMac/Xwt.Mac/ButtonBackend.cs
@@ -153,8 +153,11 @@ namespace Xwt.Mac
 			}
 		}
 
+		public void SetFormattedText (FormattedText text)
+		{
+			Widget.AttributedTitle = text.ToAttributedString ();
+		}
 
-		
 		#endregion
 
 		public override Color BackgroundColor {

commit 17247bcd0d6e57c7fff576027f60dfc48e903b93

[Gtk] Fix NRE on LabelBackend's TextIndexer
This broke with https://github.com/mono/xwt/commit/2102683398a84ee16094fc4bcae3dac44ba31872#diff-b2fc874d943bfbde146e59ed3a809b6f
which removed the assignment for TextIndexer, so trying to access it,
now that it is never assigned, results in a NRE.

Xwt.Gtk/Xwt.GtkBackend/LabelBackend.cs
@@ -39,7 +39,6 @@ namespace Xwt.GtkBackend
 	{
 		Color? textColor;
 		List<LabelLink> links;
-		TextIndexer indexer;
 
 		public LabelBackend ()
 		{
@@ -148,7 +147,6 @@ namespace Xwt.GtkBackend
 			get { return Label.Text; }
 			set {
 				links = null;
-				indexer = null;
 				Label.Text = value;
 			}
 		}
@@ -163,15 +161,15 @@ namespace Xwt.GtkBackend
 		{
 			Label.Text = text.Text;
 			formattedText = text;
-			Label.ApplyFormattedText (text);
+			var indexer = Label.ApplyFormattedText (text);
 
 			if (links != null)
 				links.Clear ();
 
 			foreach (var attr in text.Attributes.OfType<LinkTextAttribute> ()) {
 				LabelLink ll = new LabelLink () {
-					StartIndex = indexer.IndexToByteIndex (attr.StartIndex),
-					EndIndex = indexer.IndexToByteIndex (attr.StartIndex + attr.Count),
+					StartIndex = indexer != null ? indexer.IndexToByteIndex (attr.StartIndex) : attr.StartIndex,
+					EndIndex = indexer != null ? indexer.IndexToByteIndex (attr.StartIndex + attr.Count) : attr.StartIndex + attr.Count,
 					Target = attr.Target
 				};
 				if (links == null) {

Xwt.Gtk/Xwt.GtkBackend/Util.cs
@@ -281,7 +281,7 @@ namespace Xwt.GtkBackend
 		[DllImport (GtkInterop.LIBGTK, CallingConvention = CallingConvention.Cdecl)]
 		static extern void gtk_label_set_attributes (IntPtr label, IntPtr attrList);
 
-		internal static void ApplyFormattedText(this Gtk.Label label, FormattedText text)
+		internal static TextIndexer ApplyFormattedText(this Gtk.Label label, FormattedText text)
 		{
 			var list = new FastPangoAttrList ();
 			if (text != null) {
@@ -295,8 +295,12 @@ namespace Xwt.GtkBackend
 				}
 				var indexer = new TextIndexer (text.Text);
 				list.AddAttributes (indexer, text.Attributes);
+
+				return indexer;
 			}
 			gtk_label_set_attributes (label.Handle, list.Handle);
+
+			return null;
 		}
 	}
 }