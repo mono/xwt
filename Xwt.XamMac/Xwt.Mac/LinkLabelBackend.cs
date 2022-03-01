93992812f40cddff510f099cff0d8c1bd705ee69
Bret Johnson <bret.johnson@microsoft.com>
2022-03-01 00:30:49 -0500

    Fix Cocoa link labels to properly launch the browser
    Update XamMac link labels to launch the browser directly,
    in the standard way we do it elsewhere in VSMac.
    
    Fixes AB#1484767
    
    This behavior worked before with the Gtk backend, but
    I don't think it ever worked properly with the Cocoa

--- Xwt.XamMac/Xwt.Mac/LinkLabelBackend.cs
+++ Xwt.XamMac/Xwt.Mac/LinkLabelBackend.cs
@@ -102,7 +102,7 @@ namespace Xwt.Mac
 		void HandleClicked (object sender, EventArgs e)
 		{
 			ApplicationContext.InvokeUserCode (() => {
-				EventSink.OnNavigateToUrl (uri);
+				NSWorkspace.SharedWorkspace.OpenUrl (uri);
 			});
 		}
 
